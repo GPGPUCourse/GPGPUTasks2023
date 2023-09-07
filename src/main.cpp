d570bbcfc4886c91916b9bea33e8e7983c1b7705
Artem Ibragimov <artem.ibragimov1602@gmail.com>
2023-09-07 23:31:09 +0300

    todos in main.cpp are done

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,16 @@ void reportError(cl_int err, const std::string &filename, int line) {
 
 #define OCL_SAFE_CALL(expr) reportError(expr, __FILE__, __LINE__)
 
+std::string stringFrom(cl_device_type type) {
+    switch (type) {
+    case CL_DEVICE_TYPE_GPU: return "gpu";
+    case CL_DEVICE_TYPE_CPU: return "cpu";
+    case CL_DEVICE_TYPE_ACCELERATOR: return "accelerator";
+    case CL_DEVICE_TYPE_CUSTOM: return "unsupported";
+    case CL_DEVICE_TYPE_DEFAULT: return "default";
+    default: return "unknown";
+    }
+}
 
 int main() {
     // Пытаемся слинковаться с символами OpenCL API в runtime (через библиотеку libs/clew)
@@ -65,27 +75,60 @@ int main() {
         // в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
         // Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
 
+// it is done:
+//        OCL_SAFE_CALL(clGetPlatformInfo(platform, 100, 0, nullptr, &platformNameSize));
+
         // TODO 1.2
         // Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
-        std::vector<unsigned char> platformName(platformNameSize, 0);
-        // clGetPlatformInfo(...);
-        std::cout << "    Platform name: " << platformName.data() << std::endl;
+        //        std::vector<unsigned char> platformName(platformNameSize, 0);
+        const size_t dataSize = 1000;
+        char data[dataSize];
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, dataSize, data, NULL));
+        std::cout << "    Platform name: " << data << std::endl;
 
         // TODO 1.3
         // Запросите и напечатайте так же в консоль вендора данной платформы
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, dataSize, data, NULL));
+        printf("    Platform vendor: %s\n", data);
 
         // TODO 2.1
         // Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
         cl_uint devicesCount = 0;
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &devicesCount));
 
-        for (int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex) {
+        cl_device_id *devices = (cl_device_id*)calloc(devicesCount, sizeof(cl_device_id));
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices, NULL));
+
+        for (int i = 0; i < devicesCount; ++i) {
             // TODO 2.2
             // Запросите и напечатайте в консоль:
             // - Название устройства
             // - Тип устройства (видеокарта/процессор/что-то странное)
             // - Размер памяти устройства в мегабайтах
             // - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+            printf("device: #%d/%d\n", i + 1, devicesCount);
+
+            OCL_SAFE_CALL(clGetDeviceInfo(devices[i], CL_DEVICE_NAME, dataSize, data, NULL));
+            printf("    name: %s\n", data);
+
+            cl_device_type type;
+            OCL_SAFE_CALL(clGetDeviceInfo(devices[i], CL_DEVICE_TYPE, sizeof type, &type, NULL));
+            printf("    type: %s\n", stringFrom(type).data());
+
+            OCL_SAFE_CALL(clGetDeviceInfo(devices[i], CL_DEVICE_VENDOR, dataSize, data, NULL));
+            printf("    vendor: %s\n", data);
+
+            cl_ulong size;
+            OCL_SAFE_CALL(clGetDeviceInfo(devices[i], CL_DEVICE_GLOBAL_MEM_SIZE, sizeof size, &size, NULL));
+            printf("    size: %llu MB\n", size / 1024 / 1024);
+
+            cl_bool isAvailable;
+            OCL_SAFE_CALL(clGetDeviceInfo(devices[i], CL_DEVICE_AVAILABLE, sizeof isAvailable, &isAvailable, NULL));
+            printf("    available: %s\n", isAvailable ? "true" : "false");
+
         }
+
+        free(devices);
     }
 
     return 0;
