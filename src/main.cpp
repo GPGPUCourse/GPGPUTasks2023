08d6369b9000e8e19acec60a07424f027aa8fadb
lpetrov02 <lpetrov02@mail.ru>
2023-09-12 23:24:04 +0300

    Task 01 TODO 1-2

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,10 @@
 #include <cassert>
 #include <fstream>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <stdexcept>
+#include <string>
 #include <vector>
 
 
@@ -40,11 +42,97 @@ int main() {
     // TODO 1 По аналогии с предыдущим заданием узнайте, какие есть устройства, и выберите из них какое-нибудь
     // (если в списке устройств есть хоть одна видеокарта - выберите ее, если нету - выбирайте процессор)
 
+    cl_uint nPlatforms = 0;
+    OCL_SAFE_CALL(clGetPlatformIDs(0, nullptr, &nPlatforms));
+    std::cout << nPlatforms << " platforms available." << std::endl;
+
+    std::vector<cl_platform_id> platforms(nPlatforms, 0);
+    OCL_SAFE_CALL(clGetPlatformIDs(nPlatforms, platforms.data(), nullptr));
+
+    cl_device_id bestDeviceId = 0;
+    cl_platform_id bestPlatformId = 0;
+    bool gpuDeviceFound = false, deviceFound = false;
+
+    for (int i = 0; i < nPlatforms; i++) {
+        size_t platformNameSize = 0;
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 0, nullptr, &platformNameSize);
+        std::vector<unsigned char> platformName(platformNameSize, 0);
+        clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, platformNameSize, platformName.data(), nullptr);
+
+        std::cout << "Platform #" << i + 1 << "/" << nPlatforms << ":" << std::endl;
+        std::cout << "    Platform's name: " << platformName.data() << std::endl;
+
+        cl_uint devicesCount = 0;
+        OCL_SAFE_CALL(clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
+        std::cout << "    Platform's devices found: " << devicesCount << std::endl;
+
+        std::vector<cl_device_id> platformDevices(devicesCount);
+        OCL_SAFE_CALL(clGetDeviceIDs(platforms[i], CL_DEVICE_TYPE_ALL, devicesCount, platformDevices.data(), nullptr));
+
+        std::map<cl_device_type, std::string> deviceTypeMap = {
+            { (cl_device_type)(1 << 0), std::string("DEFAULT") },
+            { (cl_device_type)(1 << 1), std::string("CPU") },
+            { (cl_device_type)(1 << 2), std::string("GPU") },
+            { (cl_device_type)(1 << 3), std::string("ACCELERATOR") },
+        };
+
+        for (int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex) {
+            std::cout << "    Device #" << deviceIndex + 1 << "/" << devicesCount << std::endl;
+            cl_device_id device = platformDevices[deviceIndex];
+
+            size_t deviceNameSize = 0;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, 0, nullptr, &deviceNameSize));
+            std::vector<unsigned char> deviceName(platformNameSize, 0);
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, deviceNameSize, deviceName.data(), nullptr));
+            std::cout << "        Device name: " << deviceName.data() << std::endl;
+
+            // Тип
+            size_t deviceTypeSize = 0;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, 0, nullptr, &deviceTypeSize));
+            cl_device_type deviceType;
+            OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, deviceTypeSize, &deviceType, nullptr));
+            std::string deviceTypeStr = deviceTypeMap.find(deviceType) == deviceTypeMap.end() ? "Unknown" : deviceTypeMap[deviceType];
+            std::cout << "        Device type: " << deviceTypeStr << std::endl;
+
+            if (!gpuDeviceFound && (deviceTypeMap.find(deviceType) != deviceTypeMap.end()) && deviceTypeMap[deviceType] == "GPU") {
+                bestDeviceId = device;
+                bestPlatformId = platforms[i];
+                deviceFound = true;
+                gpuDeviceFound = true;
+            }
+            else if (!deviceFound && (deviceTypeMap.find(deviceType) != deviceTypeMap.end()) && deviceTypeMap[deviceType] == "CPU") {
+                bestDeviceId = device;
+                bestPlatformId = platforms[i];
+                deviceFound = true;
+            }
+        }
+    }
+    if (!deviceFound) {
+        std::cout << "No GPU or CPU device found" << std::endl;
+        return 1;
+    }
+
+
     // TODO 2 Создайте контекст с выбранным устройством
     // См. документацию https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/ -> OpenCL Runtime -> Contexts -> clCreateContext
     // Не забывайте проверять все возвращаемые коды на успешность (обратите внимание, что в данном случае метод возвращает
     // код по переданному аргументом errcode_ret указателю)
     // И хорошо бы сразу добавить в конце clReleaseContext (да, не очень RAII, но это лишь пример)
+    cl_int error_code = 0;
+    cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, (cl_context_properties)(bestPlatformId), 0 };
+    cl_device_id devices[1] = { bestDeviceId };
+    cl_context context;
+    context = clCreateContext(props, 1, devices, nullptr, nullptr, &error_code);
+    OCL_SAFE_CALL(error_code);
+    if (!context)
+    {
+        printf("Error: Failed to create a compute context!\n");
+        return 1;
+    }
+
+    std::cout << std::endl;
+    std::cout << "OK" << std::endl;
+    std::cout << std::endl;
 
     // TODO 3 Создайте очередь выполняемых команд в рамках выбранного контекста и устройства
     // См. документацию https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/ -> OpenCL Runtime -> Runtime APIs -> Command Queues -> clCreateCommandQueue
