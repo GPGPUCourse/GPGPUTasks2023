7d7e8c4ea1b9e188b8c0376a3e9a08491cae5f11
euskov17 <st087349@student.spbu.ru>
2023-09-12 15:57:04 +0300

    fix1

--- src/main.cpp
+++ src/main.cpp
@@ -192,7 +192,7 @@ int main() {
         // - Обращений к видеопамяти 2*n*sizeof(float) байт на чтение и 1*n*sizeof(float) байт на запись, т.е. итого 3*n*sizeof(float) байт
         // - В гигабайте 1024*1024*1024 байт
         // - Среднее время выполнения кернела равно t.lapAvg() секунд
-        std::cout << "VRAM bandwidth: " << 3 * n * sizeof(float) / t.lapAvg() / (1024*1024*1024) << " GB/s" << std::endl;
+        std::cout << "VRAM bandwidth: " << (double)3 * n * sizeof(float) / t.lapAvg() / (1024*1024*1024) << " GB/s" << std::endl;
     }
 
     // TODO 15 Скачайте результаты вычислений из видеопамяти (VRAM) в оперативную память (RAM) - из cs_gpu в cs (и рассчитайте скорость трансфера данных в гигабайтах в секунду)
@@ -200,11 +200,12 @@ int main() {
         timer t;
         for (unsigned int i = 0; i < 20; ++i) {
             cl_event read_buf;
-            clEnqueueReadBuffer(queue, cs_buf, false, 0, sizeof(cs_buf), &cs, 0, nullptr, &read_buf);
+            clEnqueueReadBuffer(queue, cs_buf, CL_TRUE, 0, sizeof(cs_buf), &cs, 0, nullptr, &read_buf);
+            OCL_SAFE_CALL(clWaitForEvents(1, &read_buf));
             t.nextLap();
         }
         std::cout << "Result data transfer time: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "VRAM -> RAM bandwidth: " << n * sizeof(float) / t.lapAvg() / (1024*1024*1024) << " GB/s" << std::endl;
+        std::cout << "VRAM -> RAM bandwidth: " << (double)n * sizeof(float) / t.lapAvg() / (1024*1024*1024) << " GB/s" << std::endl;
     }
 
     // TODO 16 Сверьте результаты вычислений со сложением чисел на процессоре (и убедитесь, что если в кернеле сделать намеренную ошибку, то эта проверка поймает ошибку)
