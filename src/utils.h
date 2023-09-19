437565d2211ded035d70f4cb89bfd2d52f3ff1b4
RazerFord <desk10567@gmail.com>
2023-09-19 16:14:00 +0300

    fix: fixed

--- src/utils.h
+++ src/utils.h
@@ -3,17 +3,9 @@
 #include <string>
 
 namespace utils {
-    inline std::string &ltrim(std::string &s) {
+    inline std::string &trim(std::string &s) {
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-        return s;
-    }
-
-    inline std::string &rtrim(std::string &s) {
         s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
         return s;
-    }
-
-    inline std::string &trim(std::string &s) {
-        return ltrim(rtrim(s));
     }
 }// namespace utils
