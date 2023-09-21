7a9adc8d57498ae271a7b5c55145220f240c3a90
Mitkin Kirill <mitkin.kirill@huawei-partners.com>
2023-09-21 23:50:36 +0300

    done

--- src/cl/mandelbrot.cl
+++ src/cl/mandelbrot.cl
@@ -4,10 +4,50 @@
 
 #line 6
 
-__kernel void mandelbrot(...)
+float mandelbrot_at_point(float x0, float y0, float iters, const float threshold) {
+    float x = x0;
+    float y = y0;
+
+    int iter = 0;
+    for (; iter < iters; ++iter) {
+        float xPrev = x;
+        x = x * x - y * y + x0;
+        y = 2.0f * xPrev * y + y0;
+        if ((x * x + y * y) > threshold) {
+            break;
+        }
+    }
+    return (float)iter / iters;
+}
+
+__kernel void mandelbrot(__global float * results,
+                         unsigned int width, unsigned int height,
+                         float fromX, float fromY,
+                         float sizeX, float sizeY,
+                         unsigned int iters, unsigned int levelAliasing)
 {
     // TODO если хочется избавиться от зернистости и дрожания при интерактивном погружении, добавьте anti-aliasing:
     // грубо говоря, при anti-aliasing уровня N вам нужно рассчитать не одно значение в центре пикселя, а N*N значений
     // в узлах регулярной решетки внутри пикселя, а затем посчитав среднее значение результатов - взять его за результат для всего пикселя
     // это увеличит число операций в N*N раз, поэтому при рассчетах гигаплопс антиальясинг должен быть выключен
+
+    int id = get_global_id(0);
+    int idx = id % width, idy = id / width;
+
+    if (idy >= height) {
+        return;
+    }
+
+    const float threshold = 256.0f;
+    const float threshold2 = threshold * threshold;
+
+    float res = 0;
+    for (int i = 0; i < levelAliasing; i++) {
+        for (int j = 0; j < levelAliasing; j++) {
+            float x0 = fromX + (idx + (1.0f + i) / (1.0f + levelAliasing)) * sizeX / width;
+            float y0 = fromY + (idy + (1.0f + j) / (1.0f + levelAliasing)) * sizeY / height;
+            res += mandelbrot_at_point(x0, y0, iters, threshold2);
+        }
+    }
+    results[id] = res / (levelAliasing * levelAliasing);
 }
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -2,6 +2,11 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#define VALUES_PER_WORKITEM 32
+#define WORKGROUP_SIZE 128
+#include "cl/sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,6 +19,7 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+const unsigned int zero = 0;
 
 int main(int argc, char **argv)
 {
@@ -59,6 +65,130 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+        gpu::gpu_mem_32u as_gpu, bs_gpu, res_gpu;
+
+        unsigned int global_work_size = (n + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE * WORKGROUP_SIZE;
+
+        as_gpu.resizeN(n);
+        bs_gpu.resizeN(n);
+        res_gpu.resizeN(1);
+        as_gpu.writeN(as.data(), n);
+
+        {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_global_atomic");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                res_gpu.writeN(&zero, 1);
+                sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                        as_gpu, res_gpu, n);
+                res_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_global_atomic) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU global atomic: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU global atomic: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+
+        }
+
+        {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_cycle");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                res_gpu.writeN(&zero, 1);
+                sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                        as_gpu, res_gpu, n);
+                res_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_cycle) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU cycle: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU cycle: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
+
+        {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_cycle_coalesced");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                res_gpu.writeN(&zero, 1);
+                sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                        as_gpu, res_gpu, n);
+                res_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_cycle_coalesced) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU cycle coalesced: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU cycle coalesced: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
+
+        {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_local_memory");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                res_gpu.writeN(&zero, 1);
+                sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                        as_gpu, res_gpu, n);
+                res_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_local_memory) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU local memory: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU local memory: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
+
+       {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_atomic_tree");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                res_gpu.writeN(&zero, 1);
+                sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                        as_gpu, res_gpu, n);
+                res_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_atomic_tree) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU atomic tree: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU atomic tree: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
+
+
+       {
+            ocl::Kernel sum(sum_kernel, sum_kernel_length, "sum_gpu_tree");
+            sum.compile();
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                unsigned int res = 0;
+                for (int current_n = n; current_n > 1; current_n = (current_n + WORKGROUP_SIZE - 1) / WORKGROUP_SIZE) {
+                    sum.exec(gpu::WorkSize(WORKGROUP_SIZE, global_work_size),
+                            as_gpu, bs_gpu, current_n);
+                    bs_gpu.copyToN(as_gpu, current_n);
+                }
+                as_gpu.readN(&res, 1);
+                EXPECT_THE_SAME(reference_sum, res, "GPU(sum_tree) result should be constistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU tree: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU tree: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
+
     }
 }
