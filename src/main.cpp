8d49f6a981125aa241259c3b148af4e9aec6d71d
Ivan <tikerlade@gmail.com>
2023-09-21 20:27:45 +0300

    Complete task#0

--- src/main.cpp
+++ src/main.cpp
@@ -65,28 +65,91 @@ int main() {
         // в документации подробно объясняется, какой ситуации соответствует данная ошибка, и это позволит, проверив код, понять, чем же вызвана данная ошибка (некорректным аргументом param_name)
         // Обратите внимание, что в этом же libs/clew/CL/cl.h файле указаны всевоможные defines, такие как CL_DEVICE_TYPE_GPU и т.п.
 
+        // DONE
+        // Попробовал 2023 - получил ошибку -30 (CL_INVALID_VALUE)
+
+
         // TODO 1.2
         // Аналогично тому, как был запрошен список идентификаторов всех платформ - так и с названием платформы, теперь, когда известна длина названия - его можно запросить:
         std::vector<unsigned char> platformName(platformNameSize, 0);
-        // clGetPlatformInfo(...);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_NAME, platformNameSize, platformName.data(), &platformNameSize));
         std::cout << "    Platform name: " << platformName.data() << std::endl;
 
         // TODO 1.3
         // Запросите и напечатайте так же в консоль вендора данной платформы
+        size_t vendorNameSize = 0;
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, 0, nullptr, &vendorNameSize));
+        std::vector<unsigned char> vendorName(vendorNameSize, 0);
+        OCL_SAFE_CALL(clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, vendorNameSize, vendorName.data(), &vendorNameSize));
+        std::cout << "    Vendor name: " << vendorName.data() << std::endl;
 
         // TODO 2.1
         // Запросите число доступных устройств данной платформы (аналогично тому, как это было сделано для запроса числа доступных платформ - см. секцию "OpenCL Runtime" -> "Query Devices")
         cl_uint devicesCount = 0;
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, nullptr, &devicesCount));
+        std::cout << "    Number of devices: " << devicesCount << std::endl;
+
+        std::vector<cl_device_id> devices(devicesCount);
+        OCL_SAFE_CALL(clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, devicesCount, devices.data(), &devicesCount));
 
         for (int deviceIndex = 0; deviceIndex < devicesCount; ++deviceIndex) {
-            // TODO 2.2
-            // Запросите и напечатайте в консоль:
-            // - Название устройства
-            // - Тип устройства (видеокарта/процессор/что-то странное)
-            // - Размер памяти устройства в мегабайтах
-            // - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
-        }
-    }
+                // TODO 2.2
+                // Запросите и напечатайте в консоль:
+                // - Название устройства
+                // - Тип устройства (видеокарта/процессор/что-то странное)
+                // - Размер памяти устройства в мегабайтах
+                // - Еще пару или более свойств устройства, которые вам покажутся наиболее интересными
+                cl_device_id device = devices[deviceIndex];
+                std::cout << "    Device #" << (deviceIndex + 1) << "/" << devicesCount << std::endl;
 
-    return 0;
+                // Name CL_DEVICE_NAME
+                size_t nameSize = 0;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, 0, nullptr, &nameSize));
+                std::vector<unsigned char> deviceName(nameSize);
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_NAME, nameSize, deviceName.data(), &nameSize));
+                std::cout << "        Device Name: " << deviceName.data() << std::endl;
+
+                // Type CL_DEVICE_TYPE
+                size_t typeSize = 0;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, 0, nullptr, &typeSize));
+                cl_device_type deviceType;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_TYPE, typeSize, &deviceType, 0));
+                std::cout << "        Device Type: ";
+
+                // From cl.h
+                if (deviceType == 1 << 1) {
+                    std::cout << "CPU" << std::endl;
+                } else if (deviceType == 1 << 2) {
+                    std::cout << "GPU" << std::endl;
+                } else if (deviceType == 1 << 3) {
+                    std::cout << "Accelerator" << std::endl;
+                }
+
+                // Memory in MB CL_DEVICE_GLOBAL_MEM_SIZE
+                // Размер памяти перевожу из байт в Мб
+                size_t memorySizeSize = 0;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, 0, nullptr, &memorySizeSize));
+                cl_ulong memorySize;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_GLOBAL_MEM_SIZE, memorySizeSize, &memorySize, 0));
+                std::cout << "        Memory Size (MB): " << memorySize / 1024 / 1024 << std::endl;
+
+                // Max compute units CL_DEVICE_MAX_COMPUTE_UNITS
+                // Как я понимаю это количество ядер на процессоре или количество work group на видеокарте
+                size_t maxComputeUnitsSize = 0;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, 0, nullptr, &maxComputeUnitsSize));
+                cl_ulong maxCopmuteUnits;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_COMPUTE_UNITS, maxComputeUnitsSize, &maxCopmuteUnits, 0));
+                std::cout << "        Max compute units: " << maxCopmuteUnits << std::endl;
+
+                // Max number of work items in a work group CL_DEVICE_MAX_WORK_GROUP_SIZE
+                // Максимальное количество work-item'ов в одной work-group'е
+                size_t maxGroupSizeSize = 0;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, 0, nullptr, &maxGroupSizeSize));
+                size_t maxGroupSize;
+                OCL_SAFE_CALL(clGetDeviceInfo(device, CL_DEVICE_MAX_WORK_GROUP_SIZE, maxGroupSizeSize, &maxGroupSize, 0));
+                std::cout << "        Max group size: " << maxGroupSize << std::endl;
+            }
+        }
+
+        return 0;
 }
