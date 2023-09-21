c7bf99a57d45f7ffb40fdbb6d7e4e9d80384cc6d
ksgamora <kostya.pyatigorsk@gmail.com>
2023-09-22 02:54:41 +0300

    first version of sum via gpu: atomic_add

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,7 @@
 // TODO
+__kernel void sum_gpu_atomic(__global const unsigned int *arr,
+                             __global unsigned int *sum)
+{
+    const unsigned int gid = get_global_id(0);
+    atomic_add(sum, arr[gid]);
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,11 +1,14 @@
+#include <libutils/fast_random.h>
 #include <libutils/misc.h>
 #include <libutils/timer.h>
-#include <libutils/fast_random.h>
 
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/sum_cl.h"
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -15,23 +18,22 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     int benchmarkingIters = 10;
 
-    unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
-    std::vector<unsigned int> as(n, 0);
+    uint32_t reference_sum = 0;
+    uint32_t n = 100 * 1000 * 1000;
+    std::vector<uint32_t> as(n, 0);
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
-        as[i] = (unsigned int) r.next(0, std::numeric_limits<unsigned int>::max() / n);
+        as[i] = (uint32_t) r.next(0, std::numeric_limits<uint32_t>::max() / n);
         reference_sum += as[i];
     }
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
+            uint32_t sum = 0;
             for (int i = 0; i < n; ++i) {
                 sum += as[i];
             }
@@ -39,14 +41,14 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU:     " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            unsigned int sum = 0;
-            #pragma omp parallel for reduction(+:sum)
+            uint32_t sum = 0;
+#pragma omp parallel for reduction(+ : sum)
             for (int i = 0; i < n; ++i) {
                 sum += as[i];
             }
@@ -54,11 +56,51 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU OMP: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        gpu::gpu_mem_32u as_gpu;
+        as_gpu.resizeN(n);
+        as_gpu.writeN(as.data(), n);
+
+        {
+            ocl::Kernel kernel(sum_kernel, sum_kernel_length, "sum_gpu_atomic");
+
+            bool printLog = true;
+            kernel.compile(printLog);
+            {
+                gpu::WorkSize workSize(128, n);
+
+                timer t;
+                uint32_t sum = 0;
+                gpu::gpu_mem_32u gpu_sum;
+                gpu_sum.resizeN(1);
+
+                for (int i = 0; i < benchmarkingIters; ++i) {
+                    uint32_t zero = 0;
+                    gpu_sum.writeN(&zero, 1);
+
+                    kernel.exec(workSize,
+                                as_gpu, // __global const uint32_t *as_gpu
+                                gpu_sum // __global uint32_t *sum
+                    );
+
+                    gpu_sum.readN(&sum, 1);
+                    EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+
+                    t.nextLap();
+                }
+                std::cout << "GPU atomic:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+                std::cout << "GPU atomic:     " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+            }
+        }
     }
 }
