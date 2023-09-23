52bff17b5278ed7f8c7a5610ee963373817bcf86
Илья Тюряев <tyuryaev2002@mail.ru>
2023-09-23 19:51:25 +0300

    make sum

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,88 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define VALUES_PER_WORKITEM 64
+#define WORKGROUP_SIZE 128
+
+__kernel void baseline(__global unsigned int *sum, __global const unsigned int *as, const unsigned int n) {
+    const unsigned id = get_global_id(0);
+    if (id >= n) {
+        return;
+    }
+    atomic_add(sum, as[id]);
+}
+
+__kernel void with_cycle(__global unsigned int *sum, __global const unsigned int *as, const unsigned int n) {
+    const unsigned id = get_global_id(0);
+    
+    unsigned int meg_sum = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; ++i) {
+        int idx = id * VALUES_PER_WORKITEM + i;
+        if (idx < n) {
+            meg_sum += as[idx];
+        }
+    }
+
+    atomic_add(sum, meg_sum);
+}
+
+__kernel void coalesced_with_cycle(__global unsigned int *sum, __global const unsigned int *as, const unsigned int n) {
+    const unsigned lid = get_local_id(0);
+    const unsigned gid = get_group_id(0);
+    const unsigned sz  = get_local_size(0);
+    
+    unsigned int meg_sum = 0;
+    for (int i = 0; i < VALUES_PER_WORKITEM; ++i) {
+        int idx = gid * sz * VALUES_PER_WORKITEM + lid * VALUES_PER_WORKITEM + i;
+        if (idx < n) {
+            meg_sum += as[idx];
+        }
+    }
+
+    atomic_add(sum, meg_sum);
+}
+
+__kernel void local_memory_and_main_thread(__global unsigned int *sum, __global const unsigned int *as, const unsigned int n) {
+    const unsigned lid = get_local_id(0);
+    const unsigned gid = get_global_id(0);
+
+    __local unsigned int buf[WORKGROUP_SIZE];
+
+    buf[lid] = gid < n ? as[gid] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (lid == 0) {
+        unsigned int meg_sum = 0;
+        for (int i = 0; i < WORKGROUP_SIZE; ++i) {
+            meg_sum += buf[i];
+        }
+
+        atomic_add(sum, meg_sum);
+    }
+}
+
+__kernel void with_tree(__global unsigned int *sum, __global const unsigned int *as, const unsigned int n) {
+    const unsigned lid = get_local_id(0);
+    const unsigned gid = get_global_id(0);
+
+    __local unsigned int buf[WORKGROUP_SIZE];
+
+    buf[lid] = gid < n ? as[gid] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    for (int m = WORKGROUP_SIZE; m > 1; m >>= 1) {
+        if (2 * lid < m) {
+            unsigned int a = buf[lid];
+            unsigned int b = buf[lid + m/2];
+            buf[lid] = a + b;
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (lid == 0) {
+        atomic_add(sum, buf[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,7 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
 
+#include "cl/sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -14,6 +17,27 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+void run(gpu::WorkSize &ws, std::string &kernel_name, int benchmarkingIters, gpu::gpu_mem_32u &as_gpu, gpu::gpu_mem_32u &sum_gpu, unsigned int n, unsigned int reference_sum) {
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, kernel_name);
+    bool printLog = false;
+    kernel.compile(printLog);
+
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        unsigned int sum = 0;
+
+        sum_gpu.writeN(&sum, 1);
+
+        kernel.exec(ws, sum_gpu, as_gpu, n);
+
+        sum_gpu.readN(&sum, 1);
+
+        EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+        t.nextLap();
+    }
+    std::cout << "GPU " << kernel_name << ": " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU " << kernel_name << ": " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+}
 
 int main(int argc, char **argv)
 {
@@ -59,6 +83,48 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate(); 
+
+        unsigned int n = as.size();
+        unsigned int work_group_size = 128;
+        unsigned int global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+        gpu::WorkSize ws = gpu::WorkSize(work_group_size, global_work_size);
+
+        gpu::gpu_mem_32u as_gpu, sum_gpu;
+        as_gpu.resizeN(n);
+        sum_gpu.resizeN(1);
+
+        as_gpu.writeN(as.data(), n);
+
+        std::string kernel_name;
+        {
+            kernel_name = "baseline";
+            ws = gpu::WorkSize(work_group_size, global_work_size);
+            run(ws, kernel_name, benchmarkingIters, as_gpu, sum_gpu, n, reference_sum);
+        }
+        {
+            kernel_name = "with_cycle";
+            ws = gpu::WorkSize(work_group_size, (((n + 64 - 1) / 64) + work_group_size - 1) / work_group_size * work_group_size);
+            run(ws, kernel_name, benchmarkingIters, as_gpu, sum_gpu, n, reference_sum);
+        }
+        {
+            kernel_name = "coalesced_with_cycle";
+            ws = gpu::WorkSize(work_group_size, (((n + 64 - 1) / 64) + work_group_size - 1) / work_group_size * work_group_size);
+            run(ws, kernel_name, benchmarkingIters, as_gpu, sum_gpu, n, reference_sum);
+        }
+        {
+            kernel_name = "local_memory_and_main_thread";
+            ws = gpu::WorkSize(work_group_size, global_work_size);
+            run(ws, kernel_name, benchmarkingIters, as_gpu, sum_gpu, n, reference_sum);
+        }
+        {
+            kernel_name = "with_tree";
+            ws = gpu::WorkSize(work_group_size, global_work_size);
+            run(ws, kernel_name, benchmarkingIters, as_gpu, sum_gpu, n, reference_sum);
+        }
     }
 }
