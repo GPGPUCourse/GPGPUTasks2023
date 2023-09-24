e2e0b6835a1345aa7228ad8d0ee422d62ffad89b
Dmitri Vasilev <lait1103@mail.ru>
2023-09-24 22:42:42 +0300

    done task 3

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,104 @@
-// TODO
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+__kernel void baseline(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    const unsigned int index = get_global_id(0);
+
+    if (index >= n)
+        return;
+
+    atomic_add(result, a[index]);
+}
+
+__kernel void cycle_n(__global const unsigned int *a, __global unsigned int *result, unsigned int n,
+                      unsigned int values_per_workitem) {
+    const unsigned global_index = get_global_id(0);
+
+    unsigned int sum = 0;
+    for (unsigned int i = 0; i < values_per_workitem; i++) {
+        unsigned int index = global_index * values_per_workitem + i;
+        if (index < n)
+            sum += a[index];
+    }
+
+    atomic_add(result, sum);
+}
+
+__kernel void cycle_3(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    cycle_n(a, result, n, 3);
+}
+
+__kernel void cycle_64(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    cycle_n(a, result, n, 64);
+}
+
+__kernel void cycle_coalesced_n(__global const unsigned int *a, __global unsigned int *result, unsigned int n,
+                                unsigned int values_per_workitem) {
+    const unsigned int local_index = get_local_id(0);
+    const unsigned int group_index = get_group_id(0);
+    const unsigned int local_size = get_local_size(0);
+
+    unsigned int sum = 0;
+
+    for (unsigned int i = 0; i < values_per_workitem; i++) {
+        unsigned int index = group_index * local_size * values_per_workitem + i * local_size + local_index;
+        if (index < n) {
+            sum += a[index];
+        }
+    }
+
+    atomic_add(result, sum);
+}
+
+__kernel void cycle_coalesced_4(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    cycle_coalesced_n(a, result, n, 4);
+}
+
+__kernel void cycle_coalesced_64(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    cycle_coalesced_n(a, result, n, 64);
+}
+
+#define WORKGROUP_SIZE 128
+__kernel void local_mem(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    const unsigned int global_index = get_global_id(0);
+    const unsigned int local_index = get_local_id(0);
+
+    __local unsigned int buffer[WORKGROUP_SIZE];
+
+    buffer[local_index] = global_index < n ? a[global_index] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_index == 0) {
+        unsigned int sum = 0;
+        for (int i = 0; i < WORKGROUP_SIZE; i++) {
+            sum += buffer[i];
+        }
+        atomic_add(result, sum);
+    }
+}
+
+__kernel void tree(__global const unsigned int *a, __global unsigned int *result, unsigned int n) {
+    const unsigned int global_index = get_global_id(0);
+    const unsigned int local_index = get_local_id(0);
+
+    __local unsigned int buffer[WORKGROUP_SIZE];
+
+    buffer[local_index] = global_index < n ? a[global_index] : 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int nValues = WORKGROUP_SIZE; nValues > 1; nValues /= 2) {
+        if (2 * local_index < nValues) {
+            buffer[local_index] += buffer[local_index + nValues / 2];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (local_index == 0) {
+        atomic_add(result, buffer[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,11 +1,14 @@
+#include "libgpu/context.h"
+#include "libgpu/shared_device_buffer.h"
+#include <libutils/fast_random.h>
 #include <libutils/misc.h>
 #include <libutils/timer.h>
-#include <libutils/fast_random.h>
+
+#include "cl/sum_cl.h"
 
 
 template<typename T>
-void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
-{
+void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
     if (a != b) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
@@ -14,13 +17,29 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+int benchmarkingIters = 10;
 
-int main(int argc, char **argv)
-{
-    int benchmarkingIters = 10;
+void exec(const std::string &kernelName, const gpu::WorkSize &workingSize, const gpu::gpu_mem_32u &as_gpu,
+          gpu::gpu_mem_32u &result, unsigned int n, unsigned int reference_sum) {
+    ocl::Kernel kernel(sum_kernel, sum_kernel_length, kernelName);
+    kernel.compile();
+    timer t;
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        unsigned int sum = 0;
+        result.writeN(&sum, 1);
+        kernel.exec(workingSize, as_gpu, result, n);
+        result.readN(&sum, 1);
+        EXPECT_THE_SAME(sum, reference_sum, "GPU result should be consistent!");
+        t.nextLap();
+    }
 
+    std::cout << "GPU " << kernelName << ": " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "GPU " << kernelName << ": " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
+}
+
+int main(int argc, char **argv) {
     unsigned int reference_sum = 0;
-    unsigned int n = 100*1000*1000;
+    unsigned int n = 100 * 1000 * 1000;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(42);
     for (int i = 0; i < n; ++i) {
@@ -39,14 +58,14 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU:     " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             unsigned int sum = 0;
-            #pragma omp parallel for reduction(+:sum)
+#pragma omp parallel for reduction(+ : sum)
             for (int i = 0; i < n; ++i) {
                 sum += as[i];
             }
@@ -54,11 +73,30 @@ int main(int argc, char **argv)
             t.nextLap();
         }
         std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU OMP: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
-        // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        unsigned int work_group_size = 128;
+        unsigned int global_work_size = (n + work_group_size - 1) / work_group_size * work_group_size;
+
+        gpu::gpu_mem_32u as_gpu, result;
+        as_gpu.resizeN(n);
+        result.resizeN(1);
+        as_gpu.writeN(as.data(), n);
+
+        exec("baseline", gpu::WorkSize(work_group_size, global_work_size), as_gpu, result, n, reference_sum);
+        exec("cycle_3", gpu::WorkSize(work_group_size, (n + 2) / 3), as_gpu, result, n, reference_sum);
+        exec("cycle_64", gpu::WorkSize(work_group_size, (n + 63) / 64), as_gpu, result, n, reference_sum);
+        exec("cycle_coalesced_4", gpu::WorkSize(work_group_size, (n + 3) / 4), as_gpu, result, n, reference_sum);
+        exec("cycle_coalesced_64", gpu::WorkSize(work_group_size, (n + 63) / 64), as_gpu, result, n, reference_sum);
+        exec("local_mem", gpu::WorkSize(work_group_size, global_work_size), as_gpu, result, n, reference_sum);
+        exec("tree", gpu::WorkSize(work_group_size, global_work_size), as_gpu, result, n, reference_sum);
     }
 }
