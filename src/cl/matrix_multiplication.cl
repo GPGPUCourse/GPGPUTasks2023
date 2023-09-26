efbeed6c06d1357f0b59c02625c536b35a95bd14
Nikolai-SE <nikolai.stoiko@yandex.ru>
2023-09-27 00:10:56 +0300

    Tasks 4.2.0, 4.2.1 are solved

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,56 @@
-__kernel void matrix_multiplication(...)
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#define TILE_SIZE 16
+
+__kernel void matrix_multiplication_naive(__global const float *as, __global const float *bs, __global float *cs, const unsigned int M, const unsigned int K, const unsigned int N)
 {
-    // TODO
+    unsigned int i = get_global_id(0);
+    unsigned int j = get_global_id(1);
+
+    float sum = 0.0f;
+    for (int k = 0; k < K; ++k) {
+        if (i < N && j < M) {
+            sum += as[j * K + k] * bs[k * N + i];
+        }
+    }
+
+    unsigned int c_index = j * N + i;
+    if (i < N && j < M) {
+        cs[c_index] = sum;
+    }
+}
+
+__kernel void matrix_multiplication_local(__global const float *as, __global const float *bs, __global float *cs, const unsigned int M, const unsigned int K, const unsigned int N)
+{
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+    int local_i = get_local_id(0);
+    int local_j = get_local_id(1);
+
+    __local float tileA[TILE_SIZE][TILE_SIZE];
+    __local float tileB[TILE_SIZE][TILE_SIZE];
+
+    float sum = 0.0f;
+
+    for (int tileK = 0; tileK * TILE_SIZE < K; ++tileK) {
+        int a_index = j * K + (tileK * TILE_SIZE + local_i);
+        int b_index = i + N * (tileK * TILE_SIZE + local_j);
+
+        tileA[local_j][local_i] = a_index < M * K ? as[a_index] : 0;
+        tileB[local_j][local_i] = b_index < K * N ? bs[b_index] : 0;
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        for (int k = 0; k < TILE_SIZE; ++k) {
+            sum += tileA[local_j][k] * tileB[k][local_i];
+        }
+
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    if (i < N && j < M) {
+        cs[j * N + i] = sum;
+    }
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -10,6 +10,51 @@
 #include <iostream>
 #include <stdexcept>
 
+int checkMultiplication(const gpu::gpu_mem_32f& as_gpu, const gpu::gpu_mem_32f& bs_gpu, unsigned int N, unsigned int K, unsigned int M,
+                        const std::vector<float>& cs_cpu_reference,
+                        const std::string& kernelName,
+                        gpu::WorkSize workSize,
+                        int benchmarkingIters = 10) {
+    const size_t gflops = ((size_t) M * K * N * 2) / (1000 * 1000 * 1000); // умножить на два, т.к. операция сложения и умножения
+    std::vector<float> cs(M*N, 0);
+    gpu::gpu_mem_32f cs_gpu;
+    cs_gpu.resizeN(M*N);
+
+    ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length, kernelName);
+    matrix_multiplication_kernel.compile();
+
+    {
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            matrix_multiplication_kernel.exec(workSize, as_gpu, bs_gpu, cs_gpu, M, K, N);
+            t.nextLap();
+        }
+        std::cout << "GPU: " << kernelName << ": " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU: " << kernelName << ": " << gflops / t.lapAvg() << " GFlops" << std::endl;
+    }
+
+    cs_gpu.readN(cs.data(), M*N);
+
+    // Проверяем корректность результатов
+    double diff_sum = 0;
+    for (int i = 0; i < M * N; ++i) {
+        double a = cs[i];
+        double b = cs_cpu_reference[i];
+        if (a != 0.0 || b != 0.0) {
+            double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
+            diff_sum += diff;
+        }
+    }
+
+    double diff_avg = diff_sum / (M * N);
+    std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
+    if (diff_avg > 0.01) {
+        std::cerr << "Too big difference!" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
 
 int main(int argc, char **argv)
 {
@@ -58,51 +103,34 @@ int main(int argc, char **argv)
 
     const std::vector<float> cs_cpu_reference = cs;
 
-    /*
-    gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
+    gpu::gpu_mem_32f as_gpu, bs_gpu;
     as_gpu.resizeN(M*K);
     bs_gpu.resizeN(K*N);
-    cs_gpu.resizeN(M*N);
 
     as_gpu.writeN(as.data(), M*K);
     bs_gpu.writeN(bs.data(), K*N);
 
-    ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length, "matrix_multiplication");
-    matrix_multiplication_kernel.compile();
+    unsigned int groupSize = 16;
 
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
+    unsigned int workSizeX = (N + groupSize - 1) / groupSize * groupSize;
+    unsigned int workSizeY = (M + groupSize - 1) / groupSize * groupSize;
 
-            t.nextLap();
-        }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+    // 4.2.0 Реализуйте наивное умножение матриц через глобальную память
+    if (int ret = checkMultiplication(as_gpu, bs_gpu, N, K, M,
+                                  cs_cpu_reference,
+                                  "matrix_multiplication_naive",
+                                  gpu::WorkSize(groupSize, groupSize, workSizeX, workSizeY),
+                                  benchmarkingIters)) {
+        return ret;
     }
 
-    cs_gpu.readN(cs.data(), M*N);
-    */
-
-    // Проверяем корректность результатов
-    double diff_sum = 0;
-    for (int i = 0; i < M * N; ++i) {
-        double a = cs[i];
-        double b = cs_cpu_reference[i];
-        if (a != 0.0 || b != 0.0) {
-            double diff = fabs(a - b) / std::max(fabs(a), fabs(b));
-            diff_sum += diff;
-        }
-    }
-
-    double diff_avg = diff_sum / (M * N);
-    std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
-    if (diff_avg > 0.01) {
-        std::cerr << "Too big difference!" << std::endl;
-        return 1;
+    // 4.2.1 Реализуйте умножение матриц через локальную память. (на лекции это вплоть до "Умножение матриц 2: локальная память")
+    if (int ret = checkMultiplication(as_gpu, bs_gpu, N, K, M,
+                                  cs_cpu_reference,
+                                  "matrix_multiplication_local",
+                                  gpu::WorkSize(groupSize, groupSize, workSizeX, workSizeY),
+                                  benchmarkingIters)) {
+        return ret;
     }
 
     return 0;
