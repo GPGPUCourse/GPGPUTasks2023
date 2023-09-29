4831a4e7a2ff984ebed0e3e76fdaa436e2d45b43
capitanFlint129 <tekkengod@list.ru>
2023-09-29 16:03:27 +0300

    naive

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,6 @@
-__kernel void matrix_transpose(...)
-{
-    // TODO
+__kernel void matrix_transpose(__global float *as_gpu, __global float *as_t_gpu, unsigned int x_size, unsigned int y_size) {
+    const unsigned int i = get_global_id(0);
+    const unsigned int j = get_global_id(1);
+    float tmp = as_gpu[j * y_size + i];
+    as_t_gpu[i * x_size + j] = tmp;
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -1,18 +1,17 @@
-#include <libutils/misc.h>
-#include <libutils/timer.h>
-#include <libutils/fast_random.h>
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libutils/fast_random.h>
+#include <libutils/misc.h>
+#include <libutils/timer.h>
 
 #include "cl/matrix_transpose_cl.h"
 
-#include <vector>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
     gpu::Context context;
@@ -23,21 +22,21 @@ int main(int argc, char **argv)
     unsigned int M = 1024;
     unsigned int K = 1024;
 
-    std::vector<float> as(M*K, 0);
-    std::vector<float> as_t(M*K, 0);
+    std::vector<float> as(M * K, 0);
+    std::vector<float> as_t(M * K, 0);
 
-    FastRandom r(M+K);
+    FastRandom r(M + K);
     for (unsigned int i = 0; i < as.size(); ++i) {
         as[i] = r.nextf();
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << std::endl;
 
-    /*
+    // /*
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
-    as_gpu.resizeN(M*K);
-    as_t_gpu.resizeN(K*M);
+    as_gpu.resizeN(M * K);
+    as_t_gpu.resizeN(K * M);
 
-    as_gpu.writeN(as.data(), M*K);
+    as_gpu.writeN(as.data(), M * K);
 
     ocl::Kernel matrix_transpose_kernel(matrix_transpose, matrix_transpose_length, "matrix_transpose");
     matrix_transpose_kernel.compile();
@@ -46,22 +45,26 @@ int main(int argc, char **argv)
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
+            unsigned int work_group_size_x = 16;
+            unsigned int work_group_size_y = 8;
+            unsigned int global_work_size_x = M;
+            unsigned int global_work_size_y = K;
             // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
             // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
             // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
             // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
+            matrix_transpose_kernel.exec(
+                    gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x, global_work_size_y), as_gpu,
+                    as_t_gpu, M, K);
 
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << M * K / 1000.0 / 1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    as_t_gpu.readN(as_t.data(), M*K);
+    as_t_gpu.readN(as_t.data(), M * K);
 
     // Проверяем корректность результатов
     for (int j = 0; j < M; ++j) {
@@ -69,12 +72,12 @@ int main(int argc, char **argv)
             float a = as[j * K + i];
             float b = as_t[i * M + j];
             if (a != b) {
-                std::cerr << "Not the same!" << std::endl;
+                std::cerr << "Not the same! " << i << " " << j  << " : " << a << " " << b << std::endl;
                 return 1;
             }
         }
     }
-    */
+    // */
 
     return 0;
 }
