b49d8010d75803e82d842451b49c1e49535f8ca1
Daniil Shushakov <4shushakov@gmail.com>
2023-09-29 15:20:04 +0300

    done matrix multiplication

--- src/cl/matrix_multiplication.cl
+++ src/cl/matrix_multiplication.cl
@@ -1,4 +1,87 @@
-__kernel void matrix_multiplication(...)
-{
-    // TODO
+__kernel void matrix_multiplication_naive(__global const float *as, __global const float *bs, __global float *cs,
+                                          unsigned int M, unsigned int K, unsigned int N) {
+    int i = get_global_id(1), j = get_global_id(0);
+    if (i >= M || j >= N)
+        return;
+    float sum = 0;
+    for (int k = 0; k < K; k++)
+        sum += as[i * K + k] * bs[k * N + j];
+    cs[i * N + j] = sum;
+}
+
+#ifndef TILE_SIZE
+    #define TILE_SIZE 16
+#endif
+
+__kernel void matrix_multiplication_local_mem(__global const float *as, __global const float *bs, __global float *cs,
+                                              unsigned int M, unsigned int K, unsigned int N) {
+    int i = get_global_id(1), j = get_global_id(0);
+    int local_i = get_local_id(1), local_j = get_local_id(0);
+
+    __local float tile_a[TILE_SIZE][TILE_SIZE], tile_b[TILE_SIZE][TILE_SIZE];
+    float sum = 0;
+    for (int tile_idx = 0; tile_idx < K; tile_idx += TILE_SIZE) {
+        if (tile_idx + local_j < K && i < M)
+            tile_a[local_i][local_j] = as[i * K + (tile_idx + local_j)];// coalesed read as
+        if (tile_idx + local_i < K && j < N)
+            tile_b[local_i][local_j] = bs[(tile_idx + local_i) * N + j];// coalesed read bs
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        if (i < M && j < N) {
+            for (int k = 0; k < min(TILE_SIZE, (int) (K - tile_idx)); k++) {
+                sum += tile_a[local_i][k] * tile_b[k][local_j];
+            }
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    if (i < M && j < N) {
+        cs[i * N + j] = sum;
+    }
+}
+
+
+#ifndef WORK_PER_ITEM
+    #define WORK_PER_ITEM 4
+#endif
+
+__kernel void matrix_multiplication_local_mem_more_work(__global const float *as, __global const float *bs,
+                                                        __global float *cs, unsigned int M, unsigned int K,
+                                                        unsigned int N) {
+    // assume TILE_SIZE % WORK_PER_ITEM == 0
+    const int ROW_TILE_SIZE = TILE_SIZE / WORK_PER_ITEM;
+    int local_i = get_local_id(1), local_j = get_local_id(0);
+    int i = get_group_id(1) * TILE_SIZE + local_i, j = get_global_id(0);
+
+    __local float tile_a[TILE_SIZE][TILE_SIZE], tile_b[TILE_SIZE][TILE_SIZE];
+
+    float sums[WORK_PER_ITEM];
+    for (int i = 0; i < WORK_PER_ITEM; i++)
+        sums[i] = 0.f;
+
+    for (int tile_idx = 0; tile_idx < K; tile_idx += TILE_SIZE) {
+        for (int w = 0; w < TILE_SIZE; w += ROW_TILE_SIZE) {
+            if (tile_idx + local_j < K && (i + w) < M)
+                tile_a[local_i + w][local_j] = as[(i + w) * K + (tile_idx + local_j)];// coalesed read as
+            if ((tile_idx + local_i + w) < K && j < N)
+                tile_b[local_i + w][local_j] = bs[(tile_idx + local_i + w) * N + j];// coalesed read bs
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+
+        for (int k = 0; k < min(TILE_SIZE, (int) (K - tile_idx)); k++) {
+            float tile_b_value = tile_b[k][local_j];
+            for (int w = 0; w < WORK_PER_ITEM; w++) {
+                if (i + w * ROW_TILE_SIZE < M && j < N) {
+                    sums[w] += tile_a[local_i + w * ROW_TILE_SIZE][k] * tile_b_value;
+                }
+            }
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+    for (int w = 0; w < WORK_PER_ITEM; w++) {
+        if (i + w * ROW_TILE_SIZE < M && j < N) {
+            cs[(i + w * ROW_TILE_SIZE) * N + j] = sums[w];
+        }
+    }
 }
--- src/main_matrix_multiplication.cpp
+++ src/main_matrix_multiplication.cpp
@@ -1,91 +1,61 @@
-#include <libutils/misc.h>
-#include <libutils/timer.h>
-#include <libutils/fast_random.h>
 #include <libgpu/context.h>
 #include <libgpu/shared_device_buffer.h>
+#include <libutils/fast_random.h>
+#include <libutils/misc.h>
+#include <libutils/timer.h>
 
 #include "cl/matrix_multiplication_cl.h"
 
-#include <vector>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+
+int benchmarkingIters = 1;// TODO пока тестируетесь удобно выставить единицу
+unsigned int M = 1024;
+unsigned int K = 1024;
+unsigned int N = 1024;
+
+const size_t gflops =
+        ((size_t) M * K * N * 2) / (1000 * 1000 * 1000);// умножить на два, т.к. операция сложения и умножения
 
 
-int main(int argc, char **argv)
-{
-    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
-
-    gpu::Context context;
-    context.init(device.device_id_opencl);
-    context.activate();
-
-    int benchmarkingIters = 10; // TODO пока тестируетесь удобно выставить единицу
-    unsigned int M = 1024;
-    unsigned int K = 1024;
-    unsigned int N = 1024;
-    const size_t gflops = ((size_t) M * K * N * 2) / (1000 * 1000 * 1000); // умножить на два, т.к. операция сложения и умножения
-
-    std::vector<float> as(M*K, 0);
-    std::vector<float> bs(K*N, 0);
-    std::vector<float> cs(M*N, 0);
-
-    FastRandom r(M+K+N);
-    for (unsigned int i = 0; i < as.size(); ++i) {
-        as[i] = r.nextf();
-    }
-    for (unsigned int i = 0; i < bs.size(); ++i) {
-        bs[i] = r.nextf();
-    }
-    std::cout << "Data generated for M=" << M << ", K=" << K << ", N=" << N << std::endl;
-
-    {
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            for (int j = 0; j < M; ++j) {
-                for (int i = 0; i < N; ++i) {
-                    float sum = 0.0f;
-                    for (int k = 0; k < K; ++k) {
-                        sum += as.data()[j * K + k] * bs.data()[k * N + i];
-                    }
-                    cs.data()[j * N + i] = sum;
+std::vector<float> cpu_compute(std::vector<float> const &as, std::vector<float> const &bs) {
+    timer t;
+    std::vector<float> cs(M * N, 0);
+    for (int iter = 0; iter < benchmarkingIters; ++iter) {
+        for (int j = 0; j < M; ++j) {
+            for (int i = 0; i < N; ++i) {
+                float sum = 0.0f;
+                for (int k = 0; k < K; ++k) {
+                    sum += as.data()[j * K + k] * bs.data()[k * N + i];
                 }
+                cs.data()[j * N + i] = sum;
             }
-            t.nextLap();
         }
-        std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+        t.nextLap();
     }
+    std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    std::cout << "CPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+    return cs;
+}
 
-    const std::vector<float> cs_cpu_reference = cs;
-
-    /*
-    gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
-    as_gpu.resizeN(M*K);
-    bs_gpu.resizeN(K*N);
-    cs_gpu.resizeN(M*N);
-
-    as_gpu.writeN(as.data(), M*K);
-    bs_gpu.writeN(bs.data(), K*N);
-
-    ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length, "matrix_multiplication");
-    matrix_multiplication_kernel.compile();
-
+void bench_and_test(ocl::Kernel kernel, gpu::WorkSize workSize, gpu::gpu_mem_32f as_gpu, gpu::gpu_mem_32f bs_gpu,
+                    gpu::gpu_mem_32f cs_gpu, std::vector<float> const &cs_cpu_reference, std::string test_name) {
+    std::cout << "Test " << test_name << ":\n";
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
-            matrix_multiplication_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, bs_gpu, cs_gpu, M, K, N);
 
+            kernel.exec(workSize, as_gpu, bs_gpu, cs_gpu, M, K, N);
             t.nextLap();
         }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
+        std::cout << "\tGPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "\tGPU: " << gflops / t.lapAvg() << " GFlops" << std::endl;
     }
 
-    cs_gpu.readN(cs.data(), M*N);
-    */
+    std::vector<float> cs = cs_cpu_reference;
+    cs_gpu.readN(cs.data(), M * N);
+
 
     // Проверяем корректность результатов
     double diff_sum = 0;
@@ -99,10 +69,79 @@ int main(int argc, char **argv)
     }
 
     double diff_avg = diff_sum / (M * N);
-    std::cout << "Average difference: " << diff_avg * 100.0 << "%" << std::endl;
+    std::cout << "\tAverage difference: " << diff_avg * 100.0 << "%" << std::endl;
     if (diff_avg > 0.01) {
-        std::cerr << "Too big difference!" << std::endl;
-        return 1;
+        std::cerr << "\tToo big difference!" << std::endl;
+        return;
+    }
+    std::cout << std::endl;
+}
+
+int main(int argc, char **argv) {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    std::vector<float> as(M * K, 0);
+    std::vector<float> bs(K * N, 0);
+
+    FastRandom r(M + K + N);
+    for (unsigned int i = 0; i < as.size(); ++i) {
+        as[i] = r.nextf();
+    }
+    for (unsigned int i = 0; i < bs.size(); ++i) {
+        bs[i] = r.nextf();
+    }
+
+    std::cout << "Data generated for M=" << M << ", K=" << K << ", N=" << N << std::endl;
+
+    const std::vector<float> cs_cpu_reference = cpu_compute(as, bs);
+
+    gpu::gpu_mem_32f as_gpu, bs_gpu, cs_gpu;
+    as_gpu.resizeN(M * K);
+    bs_gpu.resizeN(K * N);
+    cs_gpu.resizeN(M * N);
+
+    as_gpu.writeN(as.data(), M * K);
+    bs_gpu.writeN(bs.data(), K * N);
+
+    {
+        for (int work_group_size : {8, 16}) {
+            ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length,
+                                                     "matrix_multiplication_naive");
+            matrix_multiplication_kernel.compile();
+            bench_and_test(matrix_multiplication_kernel, gpu::WorkSize(work_group_size, work_group_size, N, M), as_gpu,
+                           bs_gpu, cs_gpu, cs_cpu_reference, "Naive " + std::to_string(work_group_size));
+        }
+    }
+
+    {
+        for (int work_group_size : {8, 16}) {
+            ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length,
+                                                     "matrix_multiplication_local_mem",
+                                                     "-DTILE_SIZE=" + std::to_string(work_group_size));
+            matrix_multiplication_kernel.compile();
+            bench_and_test(matrix_multiplication_kernel, gpu::WorkSize(work_group_size, work_group_size, N, M), as_gpu,
+                           bs_gpu, cs_gpu, cs_cpu_reference, "Local Mem " + std::to_string(work_group_size));
+        }
+    }
+
+    {
+        for (int work_group_size : {8, 16})
+            for (int work_per_item = 4; work_per_item <= work_group_size; work_per_item *= 2) {
+                std::string defines = "-DTILE_SIZE=" + std::to_string(work_group_size) +
+                                      " -DWORK_PER_ITEM=" + std::to_string(work_per_item);
+                ocl::Kernel matrix_multiplication_kernel(matrix_multiplication, matrix_multiplication_length,
+                                                         "matrix_multiplication_local_mem_more_work", defines);
+                matrix_multiplication_kernel.compile();
+                bench_and_test(matrix_multiplication_kernel,
+                               gpu::WorkSize(work_group_size, work_group_size / work_per_item,
+                                             (N + work_per_item - 1) / work_per_item, M),
+                               as_gpu, bs_gpu, cs_gpu, cs_cpu_reference,
+                               "More WPI " + std::to_string(work_group_size) + ", " + std::to_string(work_per_item));
+            }
     }
 
     return 0;
