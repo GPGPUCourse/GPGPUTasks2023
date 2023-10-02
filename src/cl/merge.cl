942deb842542ff988196f60366edea9a188248ad
Anton Surkis <antonsurkis@gmail.com>
2023-10-02 21:37:45 +0300

    Implement basic merge sort

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,73 @@
+uint binsearch_lt(
+    float x,
+    __global const float* arr,
+    uint len
+) {
+    if (arr[0] >= x) {
+        return 0;
+    }
+    uint l = 0;
+    uint r = len;
+    while (l + 1 < r) {
+        uint m = l + (r - l) / 2;
+        if (arr[m] < x) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    return r;
+}
 
+uint binsearch_le(
+    float x,
+    __global const float* arr,
+    uint len
+) {
+    if (arr[0] > x) {
+        return 0;
+    }
+    uint l = 0;
+    uint r = len;
+    while (l + 1 < r) {
+        uint m = l + (r - l) / 2;
+        if (arr[m] <= x) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+    return r;
+}
+
+__kernel void merge(
+    __global const float* src,
+    __global float* dst,
+    uint len,
+    uint sorted_block_len
+) {
+    uint src_flat_id = get_global_id(0);
+    if (src_flat_id >= len) {
+        return;
+    }
+
+    uint own_block = src_flat_id / sorted_block_len;
+    uint sib_block = own_block ^ 1; // *sib*ling
+
+    uint own_block_start = own_block * sorted_block_len;
+    uint sib_block_start = sib_block * sorted_block_len;
+
+    uint own_block_end = min(len, own_block_start + sorted_block_len);
+    uint sib_block_end = min(len, sib_block_start + sorted_block_len);
+
+    uint dst_flat_id = src_flat_id - own_block % 2 * sorted_block_len;
+    float x = src[src_flat_id];
+    bool is_left = own_block % 2 == 0;
+    if (is_left) {
+        dst_flat_id += binsearch_lt(x, src + sib_block_start, sib_block_end - sib_block_start);
+    } else {
+        dst_flat_id += binsearch_le(x, src + sib_block_start, sib_block_end - sib_block_start);
+    }
+
+    dst[dst_flat_id] = x;
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
+    int benchmarkingIters = 1;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
@@ -48,11 +48,13 @@ int main(int argc, char **argv) {
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << 1e-6 * n / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
@@ -61,18 +63,24 @@ int main(int argc, char **argv) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
             unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            gpu::WorkSize ws(workGroupSize, n);
+            for (cl_uint sortedSize = 1; sortedSize < n; sortedSize *= 2) {
+                merge.exec(ws, as_gpu, bs_gpu, cl_uint(n), sortedSize);
+                std::swap(as_gpu, bs_gpu);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << 1e-6 * n / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
     }
+    //for (int i = 0; i < n; ++i) {
+    //    std::cerr << "i=" << i << "\tcpu=" << cpu_sorted[i] << "\tgpu=" << as[i] << "\n";
+    //}
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
