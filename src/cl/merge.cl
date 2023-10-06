6bcc4dc63540826cda969a87f9af5f8451df8b87
Eridan Domoratskiy <eridan200@mail.ru>
2023-10-06 23:16:55 +0300

    Implement naive merge sort

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,27 @@
+// vim: syntax=c
 
+
+__kernel void merge_naive(
+    __global const float * a,
+    __global float * b,
+    unsigned int k
+) {
+    const unsigned int id = get_global_id(0);
+    const unsigned int base_idx = 2 * k * id;
+
+    __global const float * const a1 = a + base_idx;
+    __global const float * const a2 = a1 + k;
+
+    __global float * ptr = b + base_idx;
+    for (unsigned int i = 0, j = 0; i < k || j < k; ) {
+        if (i >= k || j < k && a2[j] < a1[i]) {
+            *ptr = a2[j];
+            ++j;
+        } else {
+            *ptr = a1[i];
+            ++i;
+        }
+
+        ++ptr;
+    }
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -23,6 +23,17 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
 
+static unsigned int power_of_two(unsigned int n) {
+    unsigned int x = n & (~n + 1);
+
+    while (x < n) {
+        x <<= 1;
+    }
+
+    return x;
+}
+
+
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
 
@@ -30,8 +41,8 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    const int benchmarkingIters = 10;
+    const unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,29 +61,73 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32f as_gpu;
-    as_gpu.resizeN(n);
+
+    const unsigned int good_n = power_of_two(n);
+    as.resize(good_n, 1.0 / 0.0);
+
+    gpu::gpu_mem_32f as_gpu, bs_gpu;
+    as_gpu.resizeN(good_n);
+    bs_gpu.resizeN(good_n);
+
     {
-        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
+        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge_naive");
         merge.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
-            t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            as_gpu.writeN(as.data(), good_n);
+
+            const unsigned int workGroupSize = 128;
+
+            // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
+            t.restart();
+
+            for (unsigned int k = 1; k <= good_n / 2; k <<= 1) {
+                const unsigned int gws = good_n / 2 / k;
+
+                merge.exec(gpu::WorkSize(std::min(workGroupSize, gws), gws), as_gpu, bs_gpu, k);
+
+                bs_gpu.copyToN(as_gpu, n);
+            }
+
             t.nextLap();
         }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+
+        std::cout << "GPU naïve: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU naïve: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
+
+        // Проверяем корректность результатов
+        for (int i = 0; i < n; ++i) {
+            EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        }
     }
-    // Проверяем корректность результатов
-    for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
-    }
-*/
+
+    // {
+    //     ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
+    //     merge.compile();
+    //     timer t;
+    //     for (int iter = 0; iter < benchmarkingIters; ++iter) {
+    //         as_gpu.writeN(as.data(), n);
+
+    //         // Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
+    //         t.restart();
+
+    //         const unsigned int workGroupSize = 128;
+    //         const unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+    //         merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+
+    //         t.nextLap();
+    //     }
+    //     std::cout << "GPU merge: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+    //     std::cout << "GPU merge: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+    //     as_gpu.readN(as.data(), n);
+
+    //     // Проверяем корректность результатов
+    //     for (int i = 0; i < n; ++i) {
+    //         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+    //     }
+    // }
+
     return 0;
 }
