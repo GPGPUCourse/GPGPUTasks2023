2932a535195b77ea29588e351bb6c34a4b3f6892
khaser <a-horohorin@mail.ru>
2023-10-07 15:49:01 +0300

    mergesort done

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,26 @@
+#define ROUNDDOWN(a, b) (a) / (b) * (b)
+#define ROUNDUP(a, b) ((a) + (b) - 1) / (b) * (b)
 
+__kernel void merge(__global const float *src, __global float* dest, unsigned int n, unsigned int sorted_bs) {
+    int id = get_global_id(0);
+    if (id >= n) return;
+
+    // src1 -- self block(containing worker), src2 -- other block
+    int src1_bl_start = ROUNDDOWN(id, sorted_bs);
+    int dest_bl_start = ROUNDDOWN(id, 2 * sorted_bs);
+    bool is_right = (src1_bl_start == dest_bl_start);
+    int src2_bl_start = src1_bl_start + (is_right ? sorted_bs : -sorted_bs);
+
+    int tl = -1;
+    int tr = sorted_bs;
+    while (tr - tl > 1) {
+        int tm = (tr + tl) / 2;
+        if (src[id] > src[src2_bl_start + tm] || (src[id] == src[src2_bl_start + tm] && is_right)) {
+            tl = tm;
+        } else {
+            tr = tm;
+        }
+    }
+
+    dest[dest_bl_start + tr + (id - src1_bl_start)] = src[id];
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -30,8 +30,9 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
     unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 10;
+
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,9 +51,11 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+    
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
@@ -60,19 +63,30 @@ int main(int argc, char **argv) {
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            
+            {
+                unsigned int workGroupSize = 128;
+                unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
+                for (int sorted_bs = 1; sorted_bs < n; sorted_bs *= 2) {
+                    merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, sorted_bs);
+
+                    std::swap(as_gpu, bs_gpu);
+                }
+
+            }
+
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
     }
+
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
