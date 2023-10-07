bed641e37caee731a4c0e0cc21d75e2d1964b5de
Илья Тюряев <tyuryaev2002@mail.ru>
2023-10-08 01:27:41 +0300

    publish task05

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,138 @@
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
 
+#line 6
+
+__kernel void merge_two_iters(
+                    __global const float* as, 
+                    __global float* ss, 
+                    const unsigned int n,
+                    const unsigned int len)
+{
+    const unsigned x = get_global_id(0);
+    
+    if (x*len >= n)
+        return;
+    
+    unsigned int l1 = x*len, r1 = l1+len/2;
+    unsigned int l2 = r1, r2 = l2 + len/2;
+    unsigned int it = x*len;
+    while(l1<r1 && l2<r2) {
+        float a = as[l1], b = as[l2];
+        ss[it++] = (a<=b?a:b);
+        if(a<=b) ++l1;
+        else ++l2;
+    }
+    while(l1<r1)
+        ss[it++] = as[l1++];
+    while(l2<r2)
+        ss[it++] = as[l2++];
+}
+
+void binary_search(
+        __global const float* as, 
+        const unsigned int l1, 
+        const unsigned int l2, 
+        const unsigned int len, 
+        const unsigned int x,
+        unsigned int* ra,
+        unsigned int* rb) 
+{
+    int l = 0, r = (x+1)*WORK_PER_WORKITEM - 1, lst_zero = 0; 
+
+    if(r>=len/2) {
+        r = len - r - 2;
+    }
+
+    if(r>=0) {
+        int sa = (x+1)*WORK_PER_WORKITEM - 1, sb = 0;
+        if(sa>=len/2) {
+            sb += sa - len/2 + 1;
+            sa = len/2 - 1;
+        }
+
+        while(r-l>=0) {
+            unsigned int mid = (l+r)>>1;
+
+            int a = sa - mid, b = sb + mid;
+            bool comp = as[l2+b] <= as[l1+a];
+
+            if(comp) {
+                l = mid + 1;
+                lst_zero = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        *ra = sa - lst_zero;
+        *rb = sb + lst_zero + 1;
+    } else {
+        *ra = len/2;
+        *rb = len/2;
+    }
+}
+
+__kernel void merge_local(
+                    __global const float* as, 
+                    __global float* ss,
+                    const unsigned int n,
+                    const unsigned int len,
+                    const unsigned int shift)
+{
+    const unsigned x  = get_global_id(0);
+
+    const unsigned int l1 = shift, r1 = l1+len/2;
+    const unsigned int l2 = r1, r2 = l2 + len/2;
+
+    unsigned int ssl1, ssr1;
+    unsigned int ssl2, ssr2;
+
+    if(x)
+        binary_search(as, l1, l2, len, x-1, &ssl1, &ssl2);
+    else
+        ssl1=ssl2=0;
+
+    binary_search(as, l1, l2, len, x, &ssr1, &ssr2);
+
+    __local float suba[WORK_PER_WORKITEM]; 
+    for(unsigned int i=0;i<WORK_PER_WORKITEM;++i) {
+        suba[i] = 0;
+    }
+
+    __local float subb[WORK_PER_WORKITEM]; 
+    for(unsigned int i=0;i<WORK_PER_WORKITEM;++i) {
+        subb[i] = 0;
+    }
+
+    for(unsigned int i=ssl1;i<ssr1;++i) {
+        suba[i-ssl1] = as[l1+i];
+    }
+
+    for(unsigned int i=ssl2;i<ssr2;++i) {
+        subb[i-ssl2] = as[l2+i];
+    }
+
+    ssr1 -= ssl1;
+    ssr2 -= ssl2;
+
+    ssl1 = 0;
+    ssl2 = 0;
+    
+    unsigned int it = l1 + x*WORK_PER_WORKITEM;
+    while(ssl1<ssr1 && ssl2<ssr2) {
+        float a = suba[ssl1], b = subb[ssl2];
+        ss[it++] = (a<=b?a:b);
+        if(a<=b) ++ssl1;
+        else ++ssl2;
+    }
+    
+    while(ssl1<ssr1) {
+        ss[it++] = suba[ssl1++];
+    }
+        
+    while(ssl2<ssr2) {
+        ss[it++] = subb[ssl2++];
+    }
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -31,7 +31,7 @@ int main(int argc, char **argv) {
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 32 * 1024 * 128;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,19 +50,70 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f ss_gpu;
     as_gpu.resizeN(n);
+    ss_gpu.resizeN(n);
     {
-        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
-        merge.compile();
+        const unsigned int work_per_workitem = 32;
+
+        ocl::Kernel merge1(
+            merge_kernel, 
+            merge_kernel_length, 
+            "merge_two_iters",
+            "-DWORK_PER_WORKITEM=" + std::to_string(work_per_workitem) 
+        );
+        merge1.compile();
+
+        ocl::Kernel merge2(
+            merge_kernel, 
+            merge_kernel_length, 
+            "merge_local",
+            "-DWORK_PER_WORKITEM=" + std::to_string(work_per_workitem)   
+        );
+        merge2.compile();
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            const unsigned int workGroupSize = 128;
+            const unsigned int warpsCount = 32; // сколько одновременно рабочих групп мы можем исполнять
+            
+            // пока есть возможность мерджить по два массива без простоя варпов - делаем так
+            // *(workGroupSize*warpsCount)
+            int h;
+            for(h=1;(1<<h)<=n/(workGroupSize*warpsCount);++h) {
+                unsigned int global_work_size = n / (1<<h);
+                //global_work_size = std::max(global_work_size, workGroupSize);
+                merge1.exec(
+                    gpu::WorkSize(workGroupSize, global_work_size), 
+                    as_gpu, 
+                    ss_gpu, 
+                    n, 
+                    (1<<h)
+                );
+                std::swap(as_gpu, ss_gpu);
+            }
+
+            // теперь будем мерджить два (уже больших) массива всеми воркитемами (каждый посчитает свой подотрезок ответа)
+            for(;(1<<h)<=n;++h) {
+                unsigned int len = (1<<h);
+                unsigned int global_work_size = len / work_per_workitem;
+                for(int j=0;j<n;j+=len) {
+                    merge2.exec(
+                        gpu::WorkSize(workGroupSize, global_work_size), 
+                        as_gpu, 
+                        ss_gpu, 
+                        n,
+                        len,
+                        j
+                    );
+                }
+                std::swap(as_gpu, ss_gpu);
+            }
+            
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -73,6 +124,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
