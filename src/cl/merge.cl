681c278436c966cc5e0e5b1d9e4cf286fd06e56e
Korinin38 <artkash@bk.ru>
2023-10-09 13:00:38 +0300

    Easiest part of task (one thing at a time)

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,56 @@
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
 
+#line 6
+
+#define SINGLE_ITEM_WORK 256
+
+void sort_single(__global float *array,
+						  const unsigned int n,
+						  const unsigned int sortedSize) {
+	const unsigned int gid = get_global_id(0);
+	float list1[sortedSize];
+	float list2[sortedSize];
+	int size1 = 0;
+	int size2 = 0;
+
+	for (int i = 0; i < sortedSize; ++i, ++size1) {
+		const unsigned int idx = gid * sortedSize * 2 + i;
+		if (idx >= n)
+			break;
+		list1[i] = array[idx];
+	}
+	for (int i = 0; i < sortedSize; ++i, ++size2) {
+		const unsigned int idx = gid * sortedSize + sortedSize + i;
+		if (idx >= n)
+			break;
+		list2[i] = array[idx];
+	}
+
+	int i = 0;
+	int j = 0;
+	__global float *arr_place = array + gid * sortedSize * 2;
+	while (i < size1) {
+		if (j >= size2) {
+			*(arr_place++) = list1[i++];
+		} else {
+			*(arr_place++) = list1[i] < list2[j] ? list1[i++] : list2[j++];
+		}
+	}
+	while (j < size2) {
+		*(arr_place++) = list2[j++];
+	}
+}
+
+__kernel void merge_sort(__global float *array,
+					__global float *buffer,
+					const unsigned int n,
+					const unsigned int sortedSize) {
+	if (sortedSize <= SINGLE_ITEM_WORK) {
+		sort_single(array, n, sortedSize);
+	} else {
+		// TODO
+		printf("I don't know what to do past this point!!! ;(\n");
+	}
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -50,11 +50,15 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
+
+#define SINGLE_ITEM_WORK 256
     {
-        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
+        ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge_sort");
         merge.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -62,17 +66,22 @@ int main(int argc, char **argv) {
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+			for (int sort_size = 1; sort_size <= n / 2; sort_size <<= 1)
+			{
+				merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, sort_size);
+				if (sort_size > SINGLE_ITEM_WORK)
+					std::swap(as_gpu, bs_gpu);
+			}
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
-        as_gpu.readN(as.data(), n);
+        bs_gpu.readN(as.data(), n);
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
