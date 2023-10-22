ad4639c039df543a1b3c6c5e7ebb36b58073fc49
B1tway <andrew.shukshov@gmail.com>
2023-10-22 22:55:55 +0200

    Finished task5

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,46 @@
+#ifdef __CLION_IDE__
+
+#include <cl/clion_defines.cl>
+#include <math.h>
+
+#endif
+
+#line 6
+
+__kernel void merge(__global float *in, __global float *out, unsigned int length, unsigned int block_size) {
+    unsigned int gid = get_global_id(0);
+
+    if (gid < length) {
+        unsigned int a_start = gid / (block_size << 1) * (block_size << 1);
+        unsigned int a_end = min(a_start + block_size, length);
+        unsigned int b_start = a_end;
+        unsigned int b_end = min(b_start + block_size, length);
+
+        if (b_start >= length) {
+            in[gid] = out[gid];
+        } else {
+
+            unsigned int a_pos = gid - a_start;
+            int l = max((int) a_pos - (int) (b_end - b_start), 0) - 1;
+            int r = min(block_size, a_pos);
+            while (l + 1 < r) {
+                int m = (l + r) / 2;
+                if (in[a_start + m] <= in[b_start + a_pos - m - 1]) {
+                    l = m;
+                } else {
+                    r = m;
+                }
+            }
+
+            unsigned int a = a_start + r;
+            unsigned int b = b_start + a_pos - r;
+
+            if (a < a_end && (b >= b_end || in[a] <= in[b])) {
+                out[gid] = in[a];
+            } else {
+                out[gid] = in[b];
+            }
+        }
+    }
+}
 
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -50,19 +50,24 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f as_gpu, bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
         timer t;
+        unsigned int workGroupSize = 128;
+        unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
+
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            for (unsigned int len = 1; len < n; len <<= 1) {
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, len);
+                as_gpu.swap(bs_gpu);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -73,6 +78,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
