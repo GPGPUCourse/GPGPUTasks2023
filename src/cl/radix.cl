ef669e266a665285ce8d6e1f8909bce40c32b87e
Danila Usachev <duesna897@gmail.com>
2023-10-28 19:53:15 +0300

    Completed task07

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,87 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#define WG_SIZE 128
+
+#define BLOCK_BITS 4
+#define BLOCK_VALUES (1 << BLOCK_BITS)
+
+#define DIGIT(value, shift) \
+    (((value) >> shift) & (BLOCK_VALUES - 1))
+
+/// \pre work group size is at least BLOCK_VALUES
+__kernel void radix_count_table(
+    int shift,
+    __global const unsigned *input,
+    __global unsigned *count_table,
+    __global unsigned *aux
+) {
+    int global_id = get_global_id(0);
+    unsigned this = input[global_id];
+    unsigned this_digit = DIGIT(this, shift);
+    int local_id = get_local_id(0);
+    __local int counts[BLOCK_VALUES];
+    if (local_id < BLOCK_VALUES) {
+        counts[local_id] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    atomic_inc(counts + this_digit);
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < BLOCK_VALUES) {
+        count_table[get_group_id(0) * BLOCK_VALUES + local_id] = counts[local_id];
+    }
+    // sort locally
+    __local unsigned group[2][WG_SIZE];
+    group[0][local_id] = this;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    int from = 0;
+    for (int blockLength = 1; blockLength < WG_SIZE; blockLength *= 2, from ^= 1) {
+        int blockBegin = local_id * blockLength * 2;
+        if (blockBegin < WG_SIZE) {
+            int i = blockBegin;
+            int j = blockBegin + blockLength;
+            int k = blockBegin;
+            while (i < blockBegin + blockLength && j < blockBegin + 2 * blockLength) {
+                if (DIGIT(group[from][i], shift) <= DIGIT(group[from][j], shift)) {
+                    group[from ^ 1][k++] = group[from][i++];
+                    continue;
+                }
+                group[from ^ 1][k++] = group[from][j++];
+            }
+            while (i < blockBegin + blockLength)
+                group[from ^ 1][k++] = group[from][i++];
+            while (j < blockBegin + 2 * blockLength)
+                group[from ^ 1][k++] = group[from][j++];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    aux[global_id] = group[from][local_id];
+}
+
+__kernel void radix_sort(
+    int shift,
+    __global unsigned *output,
+    __global unsigned *head_table_t,
+    __global const unsigned *counts_table,
+    __global unsigned *aux,
+    int n
+) {
+    int global_id = get_global_id(0);
+    int local_id = get_local_id(0);
+    int group_id = get_group_id(0);
+    unsigned this = aux[global_id];
+    unsigned this_digit = DIGIT(this, shift);
+    __local int counts[BLOCK_VALUES];
+    for (int index = local_id; index < BLOCK_VALUES; index += WG_SIZE) {
+        counts[index] = counts_table[group_id * BLOCK_VALUES + index];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id == 0) {
+        for (int index = 0; index + 1 < BLOCK_VALUES; ++index)
+            counts[index + 1] += counts[index];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    int output_index = local_id;
+    int head_index = this_digit * n / WG_SIZE + group_id;
+    output_index += head_table_t[head_index];
+    output_index -= counts[this_digit];
+    output[output_index] = this;
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -6,7 +6,9 @@
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/bitonic_cl.h"
+#include "cl/matrix_transpose_cl.h"
 #include "cl/merge_cl.h"
+#include "cl/prefix_sum_cl.h"
 #include "cl/radix_cl.h"
 
 #include <iostream>
@@ -32,13 +34,16 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned n = 1024 * 1024;
+    int benchmarkingIters = 3;
+    unsigned n = 32 * 1024 * 1024;
     std::vector<unsigned> as(n, 0);
     FastRandom r(n);
+    // std::cout << "as: ";
     for (unsigned int i = 0; i < n; ++i) {
-        as[i] = (unsigned) r.next(0, std::numeric_limits<int>::max());
+        as[i] = (unsigned) r.next(0, std::numeric_limits<int>::max()) % 4;
+        // std::cout << as[i] << ' ';
     }
+    // std::cout << std::endl;
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
     std::vector<unsigned> cpu_sorted = as;
@@ -73,31 +78,35 @@ int main(int argc, char **argv) {
             EXPECT_THE_SAME(result[i], cpu_sorted[i], "GPU " + algoName + " results should be correct!");
     };
 
-    ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonicSortStep");
-    bitonic.compile();
-    auto bitonicSort = [&](gpu::gpu_mem_32u &as_gpu) {
-        for (int blockToSortSize = 2; blockToSortSize <= n; blockToSortSize *= 2) {
-            for (int slidingBlockSize = blockToSortSize; slidingBlockSize > 1; slidingBlockSize /= 2) {
-                bitonic.exec(gpu::WorkSize(128, n / 2), as_gpu, blockToSortSize, slidingBlockSize);
+    {
+        ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonicSortStep");
+        bitonic.compile();
+        auto bitonicSort = [&](gpu::gpu_mem_32u &as_gpu) {
+            for (int blockToSortSize = 2; blockToSortSize <= n; blockToSortSize *= 2) {
+                for (int slidingBlockSize = blockToSortSize; slidingBlockSize > 1; slidingBlockSize /= 2) {
+                    bitonic.exec(gpu::WorkSize(128, n / 2), as_gpu, blockToSortSize, slidingBlockSize);
+                }
             }
-        }
-    };
-    examineGPUSortAlgo("bitonic", bitonicSort);
+        };
+        examineGPUSortAlgo("bitonic", bitonicSort);
+    }
 
-    ocl::Kernel mergesortPhase(merge_kernel, merge_kernel_length, "mergesortPhase");
-    mergesortPhase.compile();
     gpu::gpu_mem_32u aux_gpu;
     aux_gpu.resizeN(n);
-    auto mergeSortNaive = [&](gpu::gpu_mem_32u &as_gpu) {
-        gpu::gpu_mem_32u *from = &as_gpu, *to = &aux_gpu;
-        for (int blockLength = 1; blockLength < n; blockLength *= 2) {
-            mergesortPhase.exec(gpu::WorkSize(128, n), *from, *to, n, blockLength);
-            std::swap(from, to);
-        }
-        if (from == &aux_gpu)
-            aux_gpu.copyToN(as_gpu, n);
-    };
-    examineGPUSortAlgo("mergeSortNaive", mergeSortNaive);
+    {
+        ocl::Kernel mergesortPhase(merge_kernel, merge_kernel_length, "mergesortPhase");
+        mergesortPhase.compile();
+        auto mergeSortNaive = [&](gpu::gpu_mem_32u &as_gpu) {
+            gpu::gpu_mem_32u *from = &as_gpu, *to = &aux_gpu;
+            for (int blockLength = 1; blockLength < n; blockLength *= 2) {
+                mergesortPhase.exec(gpu::WorkSize(128, n), *from, *to, n, blockLength);
+                std::swap(from, to);
+            }
+            if (from == &aux_gpu)
+                aux_gpu.copyToN(as_gpu, n);
+        };
+        examineGPUSortAlgo("mergeSortNaive", mergeSortNaive);
+    }
 
     {
         static constexpr int WORK_GROUP_SIZE = 64;
@@ -121,6 +130,75 @@ int main(int argc, char **argv) {
                 aux_gpu.copyToN(as_gpu, n);
         };
         examineGPUSortAlgo("mergeSortDiagonal", mergeSortDiagonal);
+    }
+
+    {
+        static constexpr int WG_SIZE = 128;
+        static constexpr int WORD = 32;
+        static constexpr int BLOCK_BITS = 4;
+        static constexpr int BLOCK_VALUES = 1 << BLOCK_BITS;
+        static constexpr int MATRIX_TRANSPOSE_TILE_SIZE = 16;
+
+        ocl::Kernel radixCountTable(radix_kernel, radix_kernel_length, "radix_count_table");
+        radixCountTable.compile();
+        ocl::Kernel matrixTranspose(matrix_transpose_kernel, matrix_transpose_kernel_length, "matrix_transpose");
+        matrixTranspose.compile();
+        ocl::Kernel prefixSumUpSweep(prefix_sum_kernel, prefix_sum_kernel_length, "prefixSum_upSweep");
+        prefixSumUpSweep.compile();
+        ocl::Kernel prefixSumDownSweep(prefix_sum_kernel, prefix_sum_kernel_length, "prefixSum_downSweep");
+        prefixSumDownSweep.compile();
+        ocl::Kernel radixSortK(radix_kernel, radix_kernel_length, "radix_sort");
+        radixSortK.compile();
+
+        gpu::gpu_mem_32u aux;
+        aux.resizeN(n);
+        int countTableSize = n / WG_SIZE * BLOCK_VALUES;
+        gpu::gpu_mem_32u countTable;
+        countTable.resizeN(countTableSize);
+        gpu::gpu_mem_32u countTableT;
+        countTableT.resizeN(countTableSize);
+
+        auto dump = [](std::string name, gpu::gpu_mem_32u &gpu, int rows = 1) {
+            size_t N = gpu.number();
+            std::vector<unsigned> cpu(N);
+            gpu.readN(cpu.data(), N);
+            std::cout << name << ":\n";
+            int cols = N / rows;
+            for (int offset = 0; offset < N; offset += cols) {
+                for (int index = offset; index < offset + cols; ++index)
+                    std::cout << cpu[index] << ' ';
+                std::cout << std::endl;
+            }
+        };
+
+        auto radixSort = [&](gpu::gpu_mem_32u &as_gpu) {
+            timer radixFirst;
+            for (int shift = 0; shift < WORD; shift += BLOCK_BITS) {
+                radixFirst.restart();
+                radixCountTable.exec(gpu::WorkSize(WG_SIZE, n), shift, as_gpu, countTable, aux);
+                radixFirst.nextLap();
+                // dump("countTable", countTable, n / WG_SIZE);
+                // dump("aux", aux);
+                matrixTranspose.exec(gpu::WorkSize(MATRIX_TRANSPOSE_TILE_SIZE, MATRIX_TRANSPOSE_TILE_SIZE, BLOCK_VALUES,
+                                                   n / WG_SIZE),
+                                     countTable, countTableT, n / WG_SIZE, BLOCK_VALUES);
+                // dump("countTableT", countTableT, BLOCK_VALUES);
+                for (int stepSize = 2; stepSize <= countTableSize; stepSize *= 2)
+                    prefixSumUpSweep.exec(
+                            gpu::WorkSize(std::min(128, countTableSize / stepSize), countTableSize / stepSize),
+                            countTableT, stepSize);
+                for (int blockSize = countTableSize / 2; blockSize >= 2; blockSize /= 2)
+                    prefixSumDownSweep.exec(
+                            gpu::WorkSize(std::min(128, countTableSize / blockSize), countTableSize / blockSize),
+                            countTableT, blockSize);
+                // dump("countTableT", countTableT, BLOCK_VALUES);
+
+                radixSortK.exec(gpu::WorkSize(WG_SIZE, n), shift, as_gpu, countTableT, countTable, aux, n);
+                // dump("as_gpu", as_gpu);
+            }
+            std::cout << "radixFirst.lapSum() = " << radixFirst.lapAvg() * radixFirst.laps().size() << std::endl;
+        };
+        examineGPUSortAlgo("radixSort", radixSort);
     }
 
     return 0;
