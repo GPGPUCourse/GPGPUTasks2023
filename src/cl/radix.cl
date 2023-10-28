cffcfa3a89b2a0a91972692465a30d09192ff46e
Albert Skalt <askalt@tarantool.org>
2023-10-28 23:26:46 +0300

    solve task07

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,134 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+
+#line 6
+
+#define get_elem(e, offset) (((e) >> (offset)) & ((B) -1))
+
+// B = 2^k, 0 <= a[i] < B.
+__kernel void build_count(const __global unsigned *as, const unsigned offset, __global unsigned *count) {
+    unsigned gid = get_global_id(0);
+    unsigned lid = get_local_id(0);
+
+    __local unsigned cnt[B];
+    unsigned start = get_group_id(0) * B;
+    unsigned shift = get_local_size(0);
+    for (int i = lid; i < B; i += shift) {
+        cnt[i] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    unsigned e = get_elem(as[gid], offset);
+    atomic_add(&cnt[e], 1);
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int i = lid; i < B; i += shift) {
+        count[start + i] = cnt[i];
+    }
+}
+
+__kernel void dummy_prefix_sums(__global unsigned *in, __global unsigned *out, int resp) {
+    const int gid = get_global_id(0);
+
+    if (gid % resp == 0) {
+        unsigned accum = 0;
+        for (int i = gid; i < gid + resp; ++i) {
+            accum += in[i];
+            out[i] = accum;
+        }
+    }
+}
+
+// (m, k) -> (k, m)
+// (!) TILE_SIZE | m, k
+__kernel void transpose(__global const unsigned *a, __global unsigned *res, unsigned m, unsigned k) {
+    const int i = get_global_id(1);
+    const int j = get_global_id(0);
+
+    __local float tile[TILE_SIZE][TILE_SIZE];
+
+    const int li = get_local_id(1);
+    const int lj = get_local_id(0);
+    tile[lj][li] = a[i * k + j];
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    const int gi = get_group_id(1);
+    const int gj = get_group_id(0);
+
+    res[gj * TILE_SIZE * m + gi * TILE_SIZE + li * m + lj] = tile[li][lj];
+}
+
+__kernel void prefix_sum_sparse(const __global unsigned *sparse_in, __global unsigned *sparse_out, int sz, int n) {
+    int i = get_global_id(0);
+    sparse_out[i] = sparse_in[i];
+    if (i + sz < n) {
+        sparse_out[i] += sparse_in[i + sz];
+    }
+}
+
+__kernel void prefix_sum_supplement(__global unsigned *sparse, __global unsigned *res, int sz) {
+    int i = get_global_id(0);
+    int j = (i + 1) % (2 * sz);
+    if (sz == 1) {
+        res[i] = 0;
+    }
+    if (j >= sz) {
+        res[i] += sparse[j - sz];
+    }
+}
+
+inline bool comp(unsigned e1, unsigned e2, bool weak) {
+    return weak ? e1 < e2 : e1 <= e2;
+}
+
+int binary_search(const __local unsigned *a, int n, unsigned e, bool weak) {
+    int lo = -1;
+    int hi = n;
+    while (hi - lo > 1) {
+        int mid = (lo + hi) / 2;
+        if (comp(a[mid], e, weak)) {
+            lo = mid;
+        } else {
+            hi = mid;
+        }
+    }
+    return lo + 1;
+}
+
+__kernel void merge(const __global unsigned *in, __global unsigned *out, const int sorted, unsigned offset) {
+    const int i = get_global_id(0);
+
+    unsigned elem = in[i];
+
+    __local unsigned copy[WG_SIZE];
+    copy[get_local_id(0)] = get_elem(elem, offset);
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    const int block_begin = (i / (2 * sorted)) * (2 * sorted);
+    bool at_left = (i % (2 * sorted)) < sorted;
+
+    const int l = block_begin % WG_SIZE + at_left * sorted;
+
+    const int less = binary_search(copy + l, sorted, get_elem(elem, offset), at_left);
+    out[i - sorted * !at_left + less] = elem;
+}
+
+__kernel void radix_sort(const __global unsigned *in, __global unsigned *out, const __global unsigned *pf,
+                         const unsigned work_group_cnt, const unsigned offset, const __global unsigned *count_pf) {
+    const int gid = get_global_id(0);
+
+    unsigned elem = in[gid];
+    unsigned e = get_elem(elem, offset);
+
+    const int pf_index = work_group_cnt * e + get_group_id(0) - 1;
+
+    int lesser = ((pf_index >= 0) ? pf[pf_index] : 0) + (gid - gid / WG_SIZE * WG_SIZE) -
+                 ((e > 0) ? count_pf[get_group_id(0) * B + e - 1] : 0);
+
+    out[lesser] = elem;
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -7,6 +7,7 @@
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/radix_cl.h"
 
+#include <cassert>
 #include <iostream>
 #include <stdexcept>
 #include <vector>
@@ -22,6 +23,18 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+template<typename T>
+void debug(const std::string &label, const std::vector<T> &a) {
+    std::cout << label << std::endl;
+    std::cout << "[";
+    for (int i = 0; i < a.size(); ++i) {
+        if (i) {
+            std::cout << ", " << std::endl;
+        }
+        std::cout << label << "[" << i << "] = " << a[i];
+    }
+    std::cout << "]" << std::endl;
+}
 
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
@@ -50,13 +63,51 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32u as_gpu;
-    as_gpu.resizeN(n);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        const int B_LOG = 4;
+        const int B = 1 << B_LOG;
+        const int TILE_SIZE = 8;// | B, | n / WG_SIZE
+        const int WG_SIZE = 128;
+
+        assert((n / WG_SIZE) % TILE_SIZE == 0);
+        assert(B % TILE_SIZE == 0);
+
+        const std::string defines = "-D TILE_SIZE=" + std::to_string(TILE_SIZE) + " -D B=" + std::to_string(B) +
+                                    " -D WG_SIZE=" + std::to_string(WG_SIZE);
+        auto kernel = [&defines](const std::string &label) {
+            ocl::Kernel kernel(radix_kernel, radix_kernel_length, label, defines);
+            kernel.compile();
+            return kernel;
+        };
+        auto allocN = [](unsigned n) {
+            gpu::gpu_mem_32u mem;
+            mem.resizeN(n);
+            return mem;
+        };
+
+        auto build_count = kernel("build_count");
+        auto transpose = kernel("transpose");
+        auto prefix_sum_sparse = kernel("prefix_sum_sparse");
+        auto prefix_sum_supplement = kernel("prefix_sum_supplement");
+        auto merge = kernel("merge");
+        auto radix_sort = kernel("radix_sort");
+        auto dummy_prefix_sums = kernel("dummy_prefix_sums");
+
+        const unsigned counter_sz = n / WG_SIZE * B;
+        const unsigned bufSize = std::max(n, counter_sz);
+
+        auto as_gpu = allocN(bufSize);
+        auto counters_gpu = allocN(bufSize);
+        auto temp_gpu = allocN(bufSize);
+        auto temp2_gpu = allocN(bufSize);
+        auto pf_sums_gpu = allocN(bufSize);
+
+        auto counters_pf_gpu = allocN(bufSize);
+
+        auto n_ws = gpu::WorkSize(WG_SIZE, n);
+        auto counter_ws = gpu::WorkSize(WG_SIZE, counter_sz);
+        auto transpose_ws = gpu::WorkSize(TILE_SIZE, TILE_SIZE, B, n / WG_SIZE);
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -64,7 +115,34 @@ int main(int argc, char **argv) {
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            for (int offset = 0; offset < sizeof(unsigned) * 8; offset += B_LOG) {
+                // Presort blocks.
+                for (int sorted = 1; sorted < WG_SIZE; sorted *= 2) {
+                    merge.exec(n_ws, as_gpu, as_gpu, sorted, offset);
+                }
+
+                // Build counters.
+                build_count.exec(n_ws, as_gpu, offset, counters_gpu);
+
+                // Calc dummy prefix sums, hope it will run faster than regular pf.
+                dummy_prefix_sums.exec(counter_ws, counters_gpu, counters_pf_gpu, B);
+
+                // Transpose.
+                transpose.exec(transpose_ws, counters_gpu, temp_gpu, n / WG_SIZE, B);
+
+                // Calculate prefix sums.
+                for (int csz = 1; csz <= counter_sz; csz *= 2) {
+                    prefix_sum_supplement.exec(counter_ws, temp_gpu, pf_sums_gpu, csz);
+                    prefix_sum_sparse.exec(counter_ws, temp_gpu, temp2_gpu, csz, counter_sz);
+                    temp_gpu.swap(temp2_gpu);
+                }
+
+                // Radix.
+                radix_sort.exec(n_ws, as_gpu, temp_gpu, pf_sums_gpu, n / WG_SIZE, offset, counters_pf_gpu);
+                as_gpu.swap(temp_gpu);
+            }
+
+            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
@@ -76,6 +154,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
