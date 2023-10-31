01f4e86a10a5f379733ebf542b7d51f13f493ead
Artyom Peshkov <artemyipeshkov@gmail.com>
2023-10-31 16:47:01 +0300

    test3

--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -51,12 +51,17 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
 
+    unsigned int global_block_size = n / 128;
+
+    std::vector<unsigned int> cnts(global_block_size, 0);
+    std::vector<unsigned int> cnts_res(global_block_size, 0);
+    std::vector<unsigned int> cnts_pref(global_block_size, 0);
+    std::vector<unsigned int> bs(n, 0);
+
     {
         gpu::gpu_mem_32u as_gpu, bs_gpu, counters_gpu, counters_pref_gpu, counters_res_gpu;
         as_gpu.resizeN(n);
         bs_gpu.resizeN(n);
-
-        unsigned int global_block_size = n / 128;
 
         counters_gpu.resizeN(global_block_size);
         counters_pref_gpu.resizeN(global_block_size);
@@ -108,11 +113,15 @@ int main(int argc, char **argv) {
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
+        counters_gpu.readN(cnts.data(), global_block_size);
+        counters_pref_gpu.readN(cnts_res.data(), global_block_size);
+        counters_res_gpu.readN(cnts_pref.data(), global_block_size);
+        bs_gpu.readN(bs.data(), n);
     }
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        std::cout << as[i] << " " << cpu_sorted[i] << std::endl;
+        std::cout << as[i] << " " << cnts[i] << " " << cnts_res[i] << " " << cnts_pref[i] << " " << bs[i] << " " << cpu_sorted[i] << std::endl;
         // EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
 
