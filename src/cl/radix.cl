f7e3a01301bba87f472b983fca44687b4c49059c
Artem Trubnikov <amtrubnikov.nn@yandex.ru>
2023-11-04 00:08:19 +0300

    done

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,79 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#define WORKGROUP_SIZE 64
+#define BIT_STEP 4
+
+__kernel void radix_count(__global unsigned int *as, 
+                    __global unsigned int *cnt,
+                    unsigned int b) {
+    unsigned int i = get_global_id(0);
+    unsigned int j = get_local_id(0);
+    unsigned int g = get_group_id(0);
+
+    __local unsigned int cnt_buf[1 << BIT_STEP];
+
+    if (j < (1 << BIT_STEP))
+        cnt_buf[j] = 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    atomic_add(cnt_buf + ((as[i] >> b) & ((1 << BIT_STEP) - 1)), 1);
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (j < (1 << BIT_STEP))
+        cnt[(g << BIT_STEP) + j] = cnt_buf[j];
+}
+
+#define TILE_SIZE 16
+
+__kernel void matrix_transpose(__global float* a, __global float* at, unsigned int m, unsigned int k)
+{
+    int i = get_global_id(0);
+    int j = get_global_id(1);
+    __local float tile[TILE_SIZE][TILE_SIZE];
+    int local_i = get_local_id(0);
+    int local_j = get_local_id(1);
+
+    tile[local_j][local_i] = a[j * m + i];
+    barrier(CLK_LOCAL_MEM_FENCE);
+    at[((i - local_i) + local_j) * k + (j - local_j) + local_i] = tile[local_i][local_j];
+}
+
+__kernel void prefix_sum_up(__global unsigned int *as, unsigned int n, unsigned int d) {
+    unsigned int gid = get_global_id(0);
+    unsigned int k = (gid << (d + 1));
+    if (k + (1 << (d + 1)) - 1 >= n) return;
+    as[k + (1 << (d + 1)) - 1] += as[k + (1 << d) - 1];
+}
+
+__kernel void prefix_sum_down(__global unsigned int *as, unsigned int n, unsigned int d) {
+    unsigned int gid = get_global_id(0);
+    unsigned int k = (gid << (d + 1));
+    if (k + (1 << (d + 1)) - 1 >= n) return;
+    unsigned int tmp = as[k + (1 << d) - 1];
+    as[k + (1 << d) - 1] = as[k + (1 << (d + 1)) - 1];
+    as[k + (1 << (d + 1)) - 1] += tmp;
+}
+
+__kernel void set_0_as_zero(__global unsigned int *as, unsigned int n) {
+    as[n - 1] = 0;
+}
+
+__kernel void radix_sort(__global unsigned int *as, 
+                         __global unsigned int *bs,
+                         __global unsigned int *cnt,
+                         unsigned int b,
+                         unsigned int gc) {
+    unsigned int i = get_global_id(0);
+    unsigned int j = get_local_id(0);
+    unsigned int g = get_group_id(0);
+    
+    unsigned int bits = ((as[i] >> b) & ((1 << BIT_STEP) - 1));
+
+    unsigned int offset = cnt[gc * bits + g];
+    for (int k = i - j; k < i; k++) {
+        unsigned int a_bits = ((as[k] >> b) & ((1 << BIT_STEP) - 1));
+        offset += (a_bits == bits);
+    }
+
+    bs[offset] = as[i];
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -30,7 +30,7 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
+    int benchmarkingIters = 1;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
@@ -50,13 +50,28 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        ocl::Kernel radix_count(radix_kernel, radix_kernel_length, "radix_count");
+        radix_count.compile();
+
+        ocl::Kernel prefix_sum_up(radix_kernel, radix_kernel_length, "prefix_sum_up");
+        prefix_sum_up.compile();
+
+        ocl::Kernel prefix_sum_down(radix_kernel, radix_kernel_length, "prefix_sum_down");
+        prefix_sum_down.compile();
+
+        ocl::Kernel set_0_as_zero(radix_kernel, radix_kernel_length, "set_0_as_zero");
+        set_0_as_zero.compile();
+
+        ocl::Kernel matrix_transpose(radix_kernel, radix_kernel_length, "matrix_transpose");
+        matrix_transpose.compile();
+
+        ocl::Kernel radix_sort(radix_kernel, radix_kernel_length, "radix_sort");
+        radix_sort.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -64,7 +79,83 @@ int main(int argc, char **argv) {
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            unsigned int workgroup_size = 64;
+            unsigned int bit_step = 4;
+
+            unsigned int K = gpu::divup(n, workgroup_size), M = (1 << bit_step);
+
+            gpu::gpu_mem_32u bs_gpu;
+            bs_gpu.resizeN(n);
+
+            gpu::gpu_mem_32u cnt_gpu;
+            cnt_gpu.resizeN(M * K);
+
+            gpu::gpu_mem_32u cnt_gpu_t;
+            cnt_gpu_t.resizeN(M * K);
+
+            gpu::gpu_mem_32u pref_sum;
+            pref_sum.resizeN(M * K);
+            
+            std::vector<unsigned int> cnt(M * K);
+            std::vector<unsigned int> cntt(M * K);
+
+            for (unsigned int bit_shift = 0; bit_shift < 32; bit_shift += bit_step) {
+                /*as_gpu.readN(as.data(), n);
+                for (int i = 0; i < n; ++i)
+                    std::cout << as[i] << ' ';
+                std::cout << std::endl;
+                std::cout << std::endl;*/
+
+                {
+                    radix_count.exec(gpu::WorkSize(workgroup_size, gpu::divup(n, workgroup_size) * workgroup_size), 
+                        as_gpu, cnt_gpu, bit_shift);
+                }
+
+                {
+                    unsigned int work_group_size_x = 16;
+                    unsigned int work_group_size_y = 16;
+                    unsigned int global_work_size_x = gpu::divup(M, work_group_size_x) * work_group_size_x;
+                    unsigned int global_work_size_y = gpu::divup(K, work_group_size_y) * work_group_size_y;
+
+                    matrix_transpose.exec(gpu::WorkSize(work_group_size_x, work_group_size_y, global_work_size_x, global_work_size_y), 
+                        cnt_gpu, cnt_gpu_t, M, K);
+                }
+                
+                {
+                    unsigned int log_n = 32 - __builtin_clz(M * K - 1);
+                    for (int d = 0; d < log_n; d++) {
+                        prefix_sum_up.exec(gpu::WorkSize(workgroup_size, gpu::divup(M * K / (1 << d), workgroup_size) * workgroup_size),
+                            cnt_gpu_t, M * K, d);
+                    }
+                    set_0_as_zero.exec(gpu::WorkSize(workgroup_size, 1), cnt_gpu_t, M * K);
+                    for (int d = log_n - 1; d >= 0; d--) {
+                        prefix_sum_down.exec(gpu::WorkSize(workgroup_size, gpu::divup(M * K / (1 << d), workgroup_size) * workgroup_size),
+                            cnt_gpu_t, M * K, d);
+                    }
+                }
+
+                {
+                    radix_sort.exec(gpu::WorkSize(workgroup_size, gpu::divup(n, workgroup_size) * workgroup_size), 
+                        as_gpu, bs_gpu, cnt_gpu_t, bit_shift, K);
+                    bs_gpu.copyToN(as_gpu, n);
+                }
+
+                /*as_gpu.readN(as.data(), n);
+                cnt_gpu.readN(cnt.data(), M * K);
+                cnt_gpu_t.readN(cntt.data(), M * K);
+                for (int i = 0; i < n; ++i)
+                    std::cout << as[i] << ' ';
+                std::cout << std::endl;
+                for (int i = 0; i < M * K; ++i)
+                    std::cout << cnt[i] << ' ';
+                std::cout << std::endl;
+                for (int i = 0; i < M * K; ++i)
+                    std::cout << cntt[i] << ' ';
+                std::cout << std::endl;
+                std::cout << std::endl;*/
+            }
+
+            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
@@ -76,6 +167,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
