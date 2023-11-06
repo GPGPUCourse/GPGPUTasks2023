e0442aaf9ecd93251bd11fdfeb4734ba83d18dfe
Dmitri Vasilev <lait1103@mail.ru>
2023-11-06 03:26:26 +0300

    done

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,90 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#ifdef __Clocal_yON_IDE__
+    #include <local_ybgpu/opencl/cl/clocal_yon_defines.cl>
+#endif
+
+#line 5
+
+#define BITS 2
+#define VALUES (1 << BITS)
+#define value(n, block_id) ((n >> (block_id * BITS)) & (VALUES - 1))
+
+__kernel void counter(__global const unsigned int *as, __global unsigned int *counter, unsigned int block_id) {
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int group_id = get_group_id(0);
+    const unsigned int local_id = get_local_id(0);
+
+    __local unsigned int local_counter[VALUES];
+
+    if (local_id < VALUES)
+        local_counter[local_id] = 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    atomic_inc(&local_counter[value(as[global_id], block_id)]);
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (local_id < VALUES)
+        counter[group_id * VALUES + local_id] = local_counter[local_id];
+}
+
+#define WORKGROUP_SIZE_X 16
+#define WORKGROUP_SIZE_Y 16
+#define WORKGROUP_SIZE (WORKGROUP_SIZE_X * WORKGROUP_SIZE_Y)
+__kernel void matrix_transpose(__global const float *as, __global float *as_t, unsigned int K, unsigned int M) {
+    const unsigned int local_x = get_local_id(0);
+    const unsigned int local_y = get_local_id(1);
+
+    const unsigned int group_x = get_group_id(0);
+    const unsigned int group_y = get_group_id(1);
+
+    __local float buffer[WORKGROUP_SIZE];
+    unsigned int local_id = local_y * WORKGROUP_SIZE_X + local_x;
+    unsigned int global_x = group_x * WORKGROUP_SIZE_Y + local_x;
+    unsigned int global_y = group_y * WORKGROUP_SIZE_X + local_y;
+    if (global_x < K && global_y < M)
+        buffer[local_id] = as[K * global_y + global_x];
+    else
+        buffer[local_id] = 0;
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    local_id = local_x * WORKGROUP_SIZE_Y + local_y;
+    global_x = group_y * WORKGROUP_SIZE_X + local_x;
+    global_y = group_x * WORKGROUP_SIZE_Y + local_y;
+
+    if (global_x < M && global_y < K)
+        as_t[M * global_y + global_x] = buffer[local_id];
+}
+
+__kernel void calculate_prefix_parts(__global unsigned int *part_sums, unsigned int size) {
+    const unsigned int id = get_global_id(0);
+    part_sums[id * size] += part_sums[id * size + size / 2];
+}
+
+__kernel void calculate_prefix_sums(__global unsigned int *result, __global const unsigned int *part_sums,
+                                    unsigned int size) {
+    const unsigned int id = get_global_id(0) + 1;
+
+    if (id & size) {
+        result[id] += part_sums[(id - size) / size * size];
+    }
+}
+
+__kernel void radix(__global const unsigned int *as, __global const unsigned int *prefix_sums,
+                    __global unsigned int *tmp, unsigned int block_id, unsigned int size) {
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int group_id = get_group_id(0);
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int work_group_size = get_local_size(0);
+
+    unsigned int element_value = value(as[global_id], block_id);
+    unsigned int offset = prefix_sums[element_value * size + group_id];
+
+    for (int index = 0; index < local_id; ++index)
+        offset += value(as[group_id * work_group_size + index], block_id) == element_value;
+
+    tmp[offset] = as[global_id];
+}
+
+__kernel void reset(__global unsigned int *as) {
+    as[get_global_id(0)] = 0;
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -50,25 +50,73 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
-    gpu::gpu_mem_32u as_gpu;
+
+    unsigned int bits = 2;
+    unsigned int values = (1 << bits);
+    unsigned int work_group_size = 128;
+    unsigned int small_work_group_size = 16;
+
+    gpu::gpu_mem_32u as_gpu, counter_gpu, prefix_part_sums_gpu, prefix_result_gpu, tmp_gpu;
     as_gpu.resizeN(n);
+    counter_gpu.resizeN(n / work_group_size * values);
+    prefix_part_sums_gpu.resizeN(n / work_group_size * values);
+    prefix_result_gpu.resizeN(n / work_group_size * values + 1);
+    tmp_gpu.resizeN(n);
 
     {
         ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+        ocl::Kernel counter(radix_kernel, radix_kernel_length, "counter");
+        ocl::Kernel calculate_parts(radix_kernel, radix_kernel_length, "calculate_prefix_parts");
+        ocl::Kernel calculate_prefix_sums(radix_kernel, radix_kernel_length, "calculate_prefix_sums");
+        ocl::Kernel matrix_transpose(radix_kernel, radix_kernel_length, "matrix_transpose");
+        ocl::Kernel reset(radix_kernel, radix_kernel_length, "reset");
         radix.compile();
+        counter.compile();
+        calculate_parts.compile();
+        calculate_prefix_sums.compile();
+        matrix_transpose.compile();
+        reset.compile();
+        {
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                as_gpu.writeN(as.data(), n);
 
-        timer t;
-        for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+                t.nextLap();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+                for (int bit_group = 0; bit_group * bits < 32; bit_group++) {
+                    std::vector<unsigned> tmp(n);
+                    counter.exec(gpu::WorkSize(work_group_size, n), as_gpu, counter_gpu, bit_group);
 
-            t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
+                    matrix_transpose.exec(gpu::WorkSize(small_work_group_size, small_work_group_size,
+                                                        (values + small_work_group_size - 1) / small_work_group_size *
+                                                                small_work_group_size,
+                                                        (n / work_group_size + small_work_group_size - 1) /
+                                                                small_work_group_size * small_work_group_size),
+                                          counter_gpu, prefix_part_sums_gpu, values, n / work_group_size);
 
-            // TODO
+                    unsigned int tmp_n = n / work_group_size * values;
+
+                    for (int size = 1; size <= tmp_n; size *= 2) {
+                        if (size != 1)
+                            calculate_parts.exec(gpu::WorkSize(std::min(work_group_size, tmp_n / size), tmp_n / size),
+                                                 prefix_part_sums_gpu, size);
+                        calculate_prefix_sums.exec(gpu::WorkSize(work_group_size, tmp_n), prefix_result_gpu,
+                                                   prefix_part_sums_gpu, size);
+                    }
+
+                    unsigned x = 0;
+                    prefix_result_gpu.writeN(&x, 1);
+
+                    radix.exec(gpu::WorkSize(work_group_size, n), as_gpu, prefix_result_gpu, tmp_gpu, bit_group,
+                               n / work_group_size);
+
+                    reset.exec(gpu::WorkSize(work_group_size, tmp_n), prefix_result_gpu);
+                    std::swap(as_gpu, tmp_gpu);
+                }
+            }
+            t.stop();
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
-        std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
-
         as_gpu.readN(as.data(), n);
     }
 
@@ -76,6 +124,5 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
     return 0;
 }
