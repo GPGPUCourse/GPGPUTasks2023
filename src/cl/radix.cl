7c4329f34852b61bfc40a135813e698bc6ba320f
Astronomax <fgfgfb93@gmail.com>
2023-11-10 16:17:51 +0300

    Submit task07

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,154 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+inline bool
+comp(unsigned int e1, unsigned int e2, bool weak)
+{
+    return weak ? e1 <= e2 : e1 < e2;
+}
+
+__kernel void
+merge(
+    const __global unsigned int *input,
+    __global unsigned int *output,
+    unsigned int n,
+    unsigned int block_size,
+    const unsigned int offset,
+    const unsigned int mask)
+{
+    const int i = get_global_id(0);
+
+    if(i >= n) return;
+
+    int block_index = i / block_size;
+    int index_inside_block = i % block_size;
+    bool from_left_side = (index_inside_block < (block_size / 2));
+    unsigned int my_value = input[i];
+
+    int ind = block_index * block_size + from_left_side * (block_size / 2);
+    const __global unsigned int *another_block = input + ind;
+
+    int l = 0, r = l + min(block_size / 2, n - ind);
+
+    while(l < r) {
+        int mid = (l + r) / 2;
+        if(comp(((my_value >> offset) & mask), ((*(another_block + mid) >> offset) & mask), from_left_side))
+            r = mid;
+        else
+            l = mid + 1;
+    }
+
+    __global unsigned int *output_ptr = output + block_index * block_size + index_inside_block + r
+        - (!from_left_side) * (block_size / 2);
+    *output_ptr = my_value;
+}
+
+__kernel void
+count(
+    __global const unsigned int *as,
+    __global unsigned int *cs,
+    const unsigned int n,
+    const unsigned int offset,
+    const unsigned int mask)
+{
+    const unsigned i = get_global_id(0);
+    const unsigned local_id = get_local_id(0);
+    const unsigned group_id = get_group_id(0);
+
+    __local unsigned int cnt[K];
+    int work_per_item = (K + WG - 1) / WG;
+    for(int j = 0; j < work_per_item; j++) {
+        if(local_id * work_per_item + j >= K) break;
+        cnt[local_id * work_per_item + j] = 0;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if(i < n) {
+        unsigned int elem = (as[i] >> offset) & mask;
+        atomic_add(cnt + elem, 1);
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for(int j = 0; j < work_per_item; j++) {
+        int ind = local_id * work_per_item + j;
+        if(ind >= K) break;
+        cs[group_id * K + ind] = cnt[ind];
+    }
+}
+
+__kernel void update_blocks(
+    __global unsigned int *bs,
+    unsigned int block_size,
+    unsigned int n)
+{
+    int i = get_global_id(0);
+    if(i * block_size + block_size / 2 < n)
+        bs[i * block_size] += bs[i * block_size + block_size / 2];
+}
+
+__kernel void prefix_sum(
+    __global unsigned int *as,
+    __global unsigned int *bs,
+    unsigned int block_size,
+    unsigned int n)
+{
+    int i = get_global_id(0);
+    if(i < n && ((i + 1)&block_size) != 0) {
+        as[i] += bs[((i + 1) / block_size - 1) * block_size];
+    }
+}
+
+__kernel void
+matrix_transpose(
+    const __global unsigned int *a,
+    __global unsigned int *at,
+    unsigned int m,
+    unsigned int k)
+{
+    int j = get_global_id(0);
+    int i = get_global_id(1);
+    __local float tile[TILE_SIZE][TILE_SIZE];
+    int local_j = get_local_id(0);
+    int local_i = get_local_id(1);
+    tile[local_j][(local_i + local_j) % TILE_SIZE] = a[i * k + j]; //циклически сдвигаем каждую строку на номер строки вправо
+    barrier(CLK_LOCAL_MEM_FENCE);
+    at[(j - j % TILE_SIZE + local_i) * m + (i - i % TILE_SIZE + local_j)] = tile[local_i][(local_i + local_j) % TILE_SIZE];
+}
+
+__kernel void
+sub_prev_row(
+    const __global unsigned int *as,
+    __global unsigned int *bs,
+    unsigned int n,
+    unsigned int m)
+{
+    int j = get_global_id(0) % m;
+    int i = get_global_id(0) / m;
+    if(i < n) {
+        bs[i * m + j] = as[i * m + j];
+        if(i > 0) bs[i * m + j] -= as[i * m - 1];
+    }
+}
+
+__kernel void
+radix(
+    const __global unsigned int *as,
+    __global unsigned int *bs,
+    const __global unsigned int *pf,
+    const __global unsigned int *pf_local,
+    const unsigned int n,
+    const unsigned int offset,
+    const unsigned int mask)
+{
+    const unsigned i = get_global_id(0);
+    const unsigned local_id = get_local_id(0);
+    const unsigned group_id = get_group_id(0);
+    unsigned int groups_cnt = (n + WG - 1) / WG;
+
+    if(i < n) {
+        unsigned int elem = (as[i] >> offset) & mask;
+        int ind = 0;
+        if(elem > 0 || group_id > 0)
+            ind += pf[groups_cnt * elem + group_id - 1];
+        ind += local_id;
+        if(elem > 0)
+            ind -= pf_local[K * group_id + (elem - 1)];
+        bs[ind] = as[i];
+    }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -10,7 +10,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
-
+#include <cassert>
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
@@ -22,6 +22,28 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+void print_gpu_mem(const gpu::gpu_mem_32u& as_gpu, unsigned int n) {
+    std::vector<unsigned int> res(n, 0);
+    as_gpu.readN(res.data(), n);
+    std::cout << "\n";
+    for (auto a : res)
+        std::cout << a << " ";
+    std::cout << "\n";
+    std::cout << "\n";
+}
+
+void print_gpu_mem(const gpu::gpu_mem_32u& as_gpu, unsigned int n, unsigned int m) {
+    std::vector<unsigned int> res(n * m, 0);
+    as_gpu.readN(res.data(), n * m);
+
+    std::cout << "\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            std::cout << res[i * m + j] << " ";
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
 
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
@@ -30,8 +52,9 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 1;
+    unsigned int n = 1024 * 1024 * 32;// * 1024 * 32;// * 1024;
+
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,12 +73,56 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
+    gpu::gpu_mem_32u bs_gpu;
+    bs_gpu.resizeN(n);
 
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
+        unsigned int wg_merge = 4;
+        unsigned int wg_count = 4;
+        unsigned int wg_update_blocks = 32;
+        unsigned int wg_prefix_sum = 32;
+        unsigned int wg_transpose = 32;
+        unsigned int tile_size = 4;
+        unsigned int wg_sub_prev_row = 32;
+        unsigned int k = 2;
+
+        ocl::Kernel merge(radix_kernel, radix_kernel_length, "merge",
+                          "-DWG=" + std::to_string(wg_merge) + " "
+                                  + "-DK=" + std::to_string(1 << k) + " "
+                                  + "-DTILE_SIZE=" + std::to_string(tile_size));
+        merge.compile();
+        ocl::Kernel count(radix_kernel, radix_kernel_length, "count",
+                           "-DWG=" + std::to_string(wg_count) + " "
+                                   + "-DK=" + std::to_string(1 << k) + " "
+                                   + "-DTILE_SIZE=" + std::to_string(tile_size));
+        count.compile();
+        ocl::Kernel update_blocks(radix_kernel, radix_kernel_length, "update_blocks",
+                           "-DWG=" + std::to_string(wg_update_blocks) + " "
+                                   + "-DK=" + std::to_string(1 << k) + " "
+                                   + "-DTILE_SIZE=" + std::to_string(tile_size));
+        update_blocks.compile();
+        ocl::Kernel prefix_sum(radix_kernel, radix_kernel_length, "prefix_sum",
+                           "-DWG=" + std::to_string(wg_prefix_sum) + " "
+                                   + "-DK=" + std::to_string(1 << k) + " "
+                                   + "-DTILE_SIZE=" + std::to_string(tile_size));
+        prefix_sum.compile();
+        ocl::Kernel matrix_transpose(radix_kernel, radix_kernel_length, "matrix_transpose",
+                           "-DWG=" + std::to_string(wg_transpose) + " "
+                                   + "-DK=" + std::to_string(1 << k) + " "
+                                   + "-DTILE_SIZE=" + std::to_string(tile_size));
+        matrix_transpose.compile();
+        ocl::Kernel sub_prev_row(radix_kernel, radix_kernel_length, "sub_prev_row",
+                                     "-DWG=" + std::to_string(wg_sub_prev_row) + " "
+                                             + "-DK=" + std::to_string(1 << k) + " "
+                                             + "-DTILE_SIZE=" + std::to_string(tile_size));
+        matrix_transpose.compile();
+        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix",
+                                 "-DWG=" + std::to_string(wg_count) + " "
+                                         + "-DK=" + std::to_string(1 << k) + " "
+                                         + "-DTILE_SIZE=" + std::to_string(tile_size));
         radix.compile();
 
         timer t;
@@ -64,7 +131,119 @@ int main(int argc, char **argv) {
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            {
+                for(unsigned int offset = 0; offset < sizeof(unsigned int) * 8; offset += k) {
+                    {
+                        int block_size = 2;
+                        for (; block_size <= wg_count; block_size <<= 1) {
+                            unsigned int mask = (1 << k) - 1;
+                            unsigned int global_work_size = ((n + block_size - 1) / block_size) * block_size;
+                            merge.exec(gpu::WorkSize(wg_merge, global_work_size),
+                                       as_gpu, bs_gpu, n, block_size, offset, mask);
+                            std::swap(as_gpu, bs_gpu);
+                        }
+                        assert(block_size / 2 == wg_count);
+                    }
+                    {
+                        unsigned int max_elem = (1 << k);
+                        unsigned groups_cnt = (n + wg_count - 1) / wg_count;
+                        gpu::gpu_mem_32u cnt_gpu;
+                        cnt_gpu.resizeN(groups_cnt * max_elem);
+
+                        {
+                            unsigned int mask = (1 << k) - 1;
+                            unsigned int global_work_size = groups_cnt * wg_count;
+                            count.exec(gpu::WorkSize(wg_count, global_work_size),
+                                       as_gpu, cnt_gpu, n, offset, mask);
+                        }
+                        gpu::gpu_mem_32u cnt_pfsum_gpu1;
+                        cnt_pfsum_gpu1.resizeN(groups_cnt * max_elem);
+                        std::vector<unsigned int> zeros(groups_cnt * max_elem);
+                        cnt_pfsum_gpu1.writeN(zeros.data(), groups_cnt * max_elem);
+                        gpu::gpu_mem_32u blocks_gpu;
+                        blocks_gpu.resizeN(groups_cnt * max_elem);
+                        cnt_gpu.copyTo(blocks_gpu, groups_cnt * max_elem * sizeof(unsigned int));
+                        {
+                            unsigned int N = groups_cnt * max_elem;
+                            {
+                                unsigned int global_work_size =
+                                        (N + wg_prefix_sum - 1) / wg_prefix_sum * wg_prefix_sum;
+                                prefix_sum.exec(gpu::WorkSize(wg_prefix_sum, global_work_size),
+                                                cnt_pfsum_gpu1, blocks_gpu, 1, N);
+                            }
+                            for (unsigned int block_size = 2; block_size <= N; block_size <<= 1) {
+                                {
+                                    unsigned int blocks_count = N / block_size;
+                                    unsigned int global_work_size =
+                                            ((blocks_count + wg_update_blocks - 1) / wg_update_blocks) *
+                                            wg_update_blocks;
+                                    update_blocks.exec(gpu::WorkSize(wg_update_blocks, global_work_size),
+                                                       blocks_gpu, block_size, N);
+                                }
+                                {
+                                    unsigned int global_work_size =
+                                            (N + wg_prefix_sum - 1) / wg_prefix_sum * wg_prefix_sum;
+                                    prefix_sum.exec(gpu::WorkSize(wg_prefix_sum, global_work_size),
+                                                    cnt_pfsum_gpu1, blocks_gpu, block_size, N);
+                                }
+                            }
+                        }
+
+                        gpu::gpu_mem_32u cnt_t_gpu;
+                        cnt_t_gpu.resizeN(groups_cnt * max_elem);
+                        {
+                            matrix_transpose.exec(gpu::WorkSize(tile_size, tile_size, max_elem, groups_cnt),
+                                                  cnt_gpu, cnt_t_gpu, groups_cnt, max_elem);
+                        }
+
+                        gpu::gpu_mem_32u cnt_t_pfsum_gpu;
+                        cnt_t_pfsum_gpu.resizeN(groups_cnt * max_elem);
+                        cnt_t_pfsum_gpu.writeN(zeros.data(), groups_cnt * max_elem);
+                        cnt_t_gpu.copyTo(blocks_gpu, groups_cnt * max_elem * sizeof(unsigned int));
+                        {
+                            unsigned int N = groups_cnt * max_elem;
+                            {
+                                unsigned int global_work_size =
+                                        (N + wg_prefix_sum - 1) / wg_prefix_sum * wg_prefix_sum;
+                                prefix_sum.exec(gpu::WorkSize(wg_prefix_sum, global_work_size),
+                                                cnt_t_pfsum_gpu, blocks_gpu, 1, N);
+                            }
+                            for (unsigned int block_size = 2; block_size <= N; block_size <<= 1) {
+                                {
+                                    unsigned int blocks_count = N / block_size;
+                                    unsigned int global_work_size =
+                                            ((blocks_count + wg_update_blocks - 1) / wg_update_blocks) *
+                                            wg_update_blocks;
+                                    update_blocks.exec(gpu::WorkSize(wg_update_blocks, global_work_size),
+                                                       blocks_gpu, block_size, N);
+                                }
+                                {
+                                    unsigned int global_work_size =
+                                            (N + wg_prefix_sum - 1) / wg_prefix_sum * wg_prefix_sum;
+                                    prefix_sum.exec(gpu::WorkSize(wg_prefix_sum, global_work_size),
+                                                    cnt_t_pfsum_gpu, blocks_gpu, block_size, N);
+                                }
+                            }
+                        }
+
+                        gpu::gpu_mem_32u cnt_pfsum_gpu2;
+                        cnt_pfsum_gpu2.resizeN(groups_cnt * max_elem);
+                        {
+                            unsigned int global_work_size = (max_elem * groups_cnt + wg_sub_prev_row - 1) / wg_sub_prev_row * wg_sub_prev_row;
+                            sub_prev_row.exec(gpu::WorkSize(wg_sub_prev_row, global_work_size),
+                                              cnt_pfsum_gpu1, cnt_pfsum_gpu2, groups_cnt, max_elem);
+                        }
+                        {
+                            unsigned int mask = (1 << k) - 1;
+                            unsigned int global_work_size = groups_cnt * wg_count;
+                            radix.exec(gpu::WorkSize(wg_count, global_work_size),
+                                       as_gpu, bs_gpu, cnt_t_pfsum_gpu, cnt_pfsum_gpu2, n, offset, mask);
+                        }
+                        std::swap(as_gpu, bs_gpu);
+                    }
+                }
+                t.nextLap();
+            }
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
@@ -76,6 +255,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
