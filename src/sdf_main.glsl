4bfb17512014dcf62601e90bb304898e3bfb58f8
Alexander <alek.tulchinskij2011@yandex.ru>
2023-11-11 21:56:04 +0300

    Solve task08

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -1,3 +1,30 @@
+float sdRoundCone(vec3 p, float r1, float r2, float h)
+{
+  // sampling independent computations (only depend on shape)
+  float b = (r1 - r2) / h;
+  float a = sqrt(1.0 - b * b);
+
+  // sampling dependant computations
+  vec2 q = vec2(length(p.xz), p.y);
+  float k = dot(q, vec2(-b, a));
+  if(k < 0.0) return length(q) - r1;
+  if(k > a * h) return length(q - vec2(0.0, h)) - r2;
+  return dot(q, vec2(a, b)) - r1;
+}
+
+float sdVerticalCapsule(vec3 p, float h, float r)
+{
+  p.y -= clamp(p.y, 0.0, h);
+  return length(p) - r;
+}
+
+float sdCapsule(vec3 p, vec3 a, vec3 b, float r)
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp(dot(pa, ba) / dot(ba, ba), 0.0, 1.0);
+  return length(pa - ba * h) - r;
+}
+
 
 // sphere with center in (0, 0, 0)
 float sdSphere(vec3 p, float r)
@@ -15,13 +42,20 @@ float sdPlane(vec3 p)
 float lazycos(float angle)
 {
     int nsleep = 10;
-    
+
     int iperiod = int(angle / 6.28318530718) % nsleep;
     if (iperiod < 3) {
         return cos(angle);
     }
-    
+
     return 1.0;
+}
+
+// exponential smooth min (k=32)
+float smin(float a, float b, float k)
+{
+    float res = exp2(-k * a) + exp2(-k * b);
+    return -log2(res) / k;
 }
 
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
@@ -31,16 +65,59 @@ vec4 sdBody(vec3 p)
     float d = 1e10;
 
     // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
-    
+    d = sdRoundCone((p - vec3(0.0, 0.35, -0.7)), 0.35, 0.3, 0.3);
+
+    float walkCycle = cos(iTime) * 0.1;
+
+    float lLeg = sdVerticalCapsule((p - vec3(-0.2, 0.0, -0.7 + walkCycle)), 0.3, 0.07);
+    float rLeg = sdVerticalCapsule((p - vec3(0.2, 0.0, -0.7 - walkCycle)), 0.3, 0.07);
+
+    d = smin(d, lLeg, 32.0);
+    d = smin(d, rLeg, 32.0);
+
+    float rot = (1.0 - lazycos(iTime * 10.0)) * 0.2;
+    float lSwing = rot;
+
+    float lWristZ = -walkCycle;
+    if (rot != 0.0) {
+        lWristZ = 0.0;
+    }
+
+    float lHand = sdCapsule((p - vec3(-0.2, 0.0, -0.7)), vec3(-0.3, lSwing + 0.2, lWristZ), vec3(0.0, 0.5, -0.1), 0.06);
+    float rHand = sdCapsule((p - vec3(0.2, 0.0, -0.7)), vec3(0.0, 0.5, -0.1), vec3(0.3, 0.2, walkCycle), 0.06);
+
+    d = smin(d, lHand, 32.0);
+    d = smin(d, rHand, 32.0);
+
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
 }
 
 vec4 sdEye(vec3 p)
 {
+    float d = 1e10;
+    vec4 res = vec4(d, vec3(1.0, 1.0, 1.0));
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    d = sdSphere((p - vec3(0.0, 0.65, -0.5)), 0.2);
+    vec4 whiteEye = vec4(d, vec3(1.0, 1.0, 1.0));
+
+    if (whiteEye.x < res.x) {
+        res = whiteEye;
+    }
+
+    d = sdSphere((p - vec3(0.0, 0.63, -0.43)), 0.15);
+    vec4 blueEye = vec4(d, vec3(21.0 / 256.0, 105.0 / 256.0, 199.0 / 256.0));
+
+    if (blueEye.x < res.x) {
+        res = blueEye;
+    }
+
+    d = sdSphere((p - vec3(0.0, 0.61, -0.37)), 0.1);
+    vec4 blackEye = vec4(d, vec3(0.0, 0.0, 0.0));
+
+    if (blackEye.x < res.x) {
+        res = blackEye;
+    }
     
     return res;
 }
