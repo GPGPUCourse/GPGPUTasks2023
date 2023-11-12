eac5d3c391eebb8c2353d1856aac282a60cd380d
Dmitri Vasilev <lait1103@mail.ru>
2023-11-13 02:00:33 +0300

    done practice8

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -1,3 +1,6 @@
+float dot2(in vec2 v) { return dot(v, v); }
+float dot2(in vec3 v) { return dot(v, v); }
+float ndot(in vec2 a, in vec2 b) { return a.x*b.x - a.y*b.y; }
 
 // sphere with center in (0, 0, 0)
 float sdSphere(vec3 p, float r)
@@ -15,14 +18,30 @@ float sdPlane(vec3 p)
 float lazycos(float angle)
 {
     int nsleep = 10;
-    
+
     int iperiod = int(angle / 6.28318530718) % nsleep;
     if (iperiod < 3) {
         return cos(angle);
     }
-    
+
     return 1.0;
 }
+
+// exponential smooth min (k=32)
+float smin(float a, float b, float k)
+{
+    float res = exp2(-k*a) + exp2(-k*b);
+    return -log2(res)/k;
+}
+
+
+float sdCapsule(vec3 p, vec3 a, vec3 b, float r)
+{
+    vec3 pa = p - a, ba = b - a;
+    float h = clamp(dot(pa, ba)/dot(ba, ba), 0.0, 1.0);
+    return length(pa - ba*h) - r;
+}
+
 
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
 // способ сделать гладкий переход между примитивами: https://iquilezles.org/articles/smin/
@@ -30,19 +49,38 @@ vec4 sdBody(vec3 p)
 {
     float d = 1e10;
 
-    // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
-    
+
+    float hand_r = sdCapsule(p - vec3(-0.35, 0.35, -0.7), vec3(-0.35, 0.15 * lazycos(iTime * 6.0), 0.2), vec3(0.0, 0.0, 0.0), 0.05);
+    float hand_l = sdCapsule(p - vec3(0.35, 0.35, -0.7), vec3(0.35, -0.15 * lazycos(iTime * 6.0), 0.2), vec3(0.0, 0.0, 0.0), 0.05);
+    float hands = smin(hand_l, hand_r, 32.0);
+
+    float leg_l = sdSphere((p - vec3(-0.2, 0.0, -0.7)), 0.06);
+    float leg_r = sdSphere((p - vec3(0.2, 0.0, -0.7)), 0.06);
+    float legs = smin(leg_l, leg_r, 32.0);
+
+    float body = sdSphere(p - vec3(0.0, 0.35, -0.7), 0.35);
+    body = smin(body, hands, 32.0);
+    body = smin(legs, body, 32.0);
+
     // return distance and color
-    return vec4(d, vec3(0.0, 1.0, 0.0));
+    return vec4(body, vec3(0.2, 0.1, 0.7));
 }
 
 vec4 sdEye(vec3 p)
 {
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
-    
-    return res;
+    vec3 center = p - vec3(0.0, 0.5, -0.5);
+    float eye = sdSphere(center, 0.15);
+    float white_part = sdSphere(center - vec3(0.0, 0.025, 0.1), 0.08);
+    float black_part = sdSphere(center - vec3(0.0, 0.025, 0.16), 0.03);
+    if (eye < white_part) {
+        return vec4(eye, 1.0, 1.0, 1.0);
+    } else if (white_part < black_part) {
+        return vec4(white_part, 0.0, 1.0, 0.0);
+    }
+    else {
+        return vec4(black_part, 0.0, 0.0, 0.0);
+    }
 }
 
 vec4 sdMonster(vec3 p)
@@ -50,14 +88,14 @@ vec4 sdMonster(vec3 p)
     // при рисовании сложного объекта из нескольких SDF, удобно на верхнем уровне 
     // модифицировать p, чтобы двигать объект как целое
     p -= vec3(0.0, 0.08, 0.0);
-    
+
     vec4 res = sdBody(p);
-    
+
     vec4 eye = sdEye(p);
     if (eye.x < res.x) {
         res = eye;
     }
-    
+
     return res;
 }
 
@@ -65,37 +103,37 @@ vec4 sdMonster(vec3 p)
 vec4 sdTotal(vec3 p)
 {
     vec4 res = sdMonster(p);
-    
-    
+
+
     float dist = sdPlane(p);
     if (dist < res.x) {
         res = vec4(dist, vec3(1.0, 0.0, 0.0));
     }
-    
+
     return res;
 }
 
 // see https://iquilezles.org/articles/normalsSDF/
-vec3 calcNormal( in vec3 p ) // for function f(p)
+vec3 calcNormal(in vec3 p)// for function f(p)
 {
-    const float eps = 0.0001; // or some other value
-    const vec2 h = vec2(eps,0);
-    return normalize( vec3(sdTotal(p+h.xyy).x - sdTotal(p-h.xyy).x,
-                           sdTotal(p+h.yxy).x - sdTotal(p-h.yxy).x,
-                           sdTotal(p+h.yyx).x - sdTotal(p-h.yyx).x ) );
+    const float eps = 0.0001;// or some other value
+    const vec2 h = vec2(eps, 0);
+    return normalize(vec3(sdTotal(p+h.xyy).x - sdTotal(p-h.xyy).x,
+    sdTotal(p+h.yxy).x - sdTotal(p-h.yxy).x,
+    sdTotal(p+h.yyx).x - sdTotal(p-h.yyx).x));
 }
 
 
 vec4 raycast(vec3 ray_origin, vec3 ray_direction)
 {
-    
+
     float EPS = 1e-3;
-    
-    
+
+
     // p = ray_origin + t * ray_direction;
-    
+
     float t = 0.0;
-    
+
     for (int iter = 0; iter < 200; ++iter) {
         vec4 res = sdTotal(ray_origin + t*ray_direction);
         t += res.x;
@@ -110,11 +148,11 @@ vec4 raycast(vec3 ray_origin, vec3 ray_direction)
 
 float shading(vec3 p, vec3 light_source, vec3 normal)
 {
-    
+
     vec3 light_dir = normalize(light_source - p);
-    
+
     float shading = dot(light_dir, normal);
-    
+
     return clamp(shading, 0.5, 1.0);
 
 }
@@ -126,59 +164,58 @@ float specular(vec3 p, vec3 light_source, vec3 N, vec3 camera_center, float shin
     vec3 R = reflect(L, N);
 
     vec3 V = normalize(camera_center - p);
-    
+
     return pow(max(dot(R, V), 0.0), shinyness);
 }
 
 
 float castShadow(vec3 p, vec3 light_source)
 {
-    
+
     vec3 light_dir = p - light_source;
-    
+
     float target_dist = length(light_dir);
-    
-    
+
+
     if (raycast(light_source, normalize(light_dir)).x + 0.001 < target_dist) {
         return 0.5;
     }
-    
+
     return 1.0;
 }
 
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
+void mainImage(out vec4 fragColor, in vec2 fragCoord)
 {
     vec2 uv = fragCoord/iResolution.y;
-    
+
     vec2 wh = vec2(iResolution.x / iResolution.y, 1.0);
-    
+
 
     vec3 ray_origin = vec3(0.0, 0.5, 1.0);
     vec3 ray_direction = normalize(vec3(uv - 0.5*wh, -1.0));
-    
+
 
     vec4 res = raycast(ray_origin, ray_direction);
-    
-    
-    
+
+
+
     vec3 col = res.yzw;
-    
-    
+
+
     vec3 surface_point = ray_origin + res.x*ray_direction;
     vec3 normal = calcNormal(surface_point);
-    
+
     vec3 light_source = vec3(1.0 + 2.5*sin(iTime), 10.0, 10.0);
-    
+
     float shad = shading(surface_point, light_source, normal);
     shad = min(shad, castShadow(surface_point, light_source));
     col *= shad;
-    
+
     float spec = specular(surface_point, light_source, normal, ray_origin, 30.0);
     col += vec3(1.0, 1.0, 1.0) * spec;
-    
-    
-    
+
+
     // Output to screen
     fragColor = vec4(col, 1.0);
 }
