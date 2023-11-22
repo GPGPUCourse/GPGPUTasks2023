b26bc17748bc41671c65cd0a275b2d7aa3a92750
Ivan <tikerlade@gmail.com>
2023-11-22 15:20:51 +0300

    Complete task03

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,96 @@
-// TODO
+#define WG_SIZE 128
+
+__kernel void sum_atomic(__global uint* as, __global uint* res, const uint n) {
+    const uint gid = get_global_id(0);
+    if (gid >= n) {
+        return;
+    }
+
+    atomic_add(&res[0], as[gid]);
+}
+
+__kernel void sum_loop(__global uint* as, __global uint* res, const uint n) {
+    // Можно было вынести в аргументы функции, но хотелось, чтобы все функции были одинаково параметризованы
+    const uint numItems = 32;
+    const uint gid = get_global_id(0);
+    if (gid >= n) {
+        return;
+    }
+
+    uint local_res = 0;
+    for (int i = 0; i < numItems; i++) {
+        uint idx = gid * numItems + i;
+        if (idx < n) {
+            local_res += as[idx];
+        }
+    }
+
+    atomic_add(&res[0], local_res);
+}
+
+__kernel void sum_loop_coalesced(__global uint* as, __global uint* res, const uint n) {
+    const uint numIters = 32;
+    const uint lid = get_local_id(0);
+    const uint wgId = get_group_id(0);
+
+    uint local_res = 0;
+    for (int i = 0; i < numIters; ++i) {
+        uint idx = wgId * WG_SIZE * numIters + i * WG_SIZE + lid;
+        if (idx < n) {
+            local_res += as[idx];
+        }
+    }
+    
+    atomic_add(&res[0], local_res);
+}
+
+
+__kernel void sum_local(__global uint* as, __global uint* res, const uint n) {
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+    if (gid >= n) {
+        return;
+    }
+
+    __local uint a[WG_SIZE];
+    if (lid < WG_SIZE) {
+        a[lid] = as[gid];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (lid == 0) {
+        uint local_res = 0;
+        for (int i = 0; i < WG_SIZE; ++i) {
+            local_res += a[i];
+        }
+
+        atomic_add(&res[0], local_res);
+    }
+}
+
+
+__kernel void sum_tree(__global uint* as, __global uint* res, const uint n) {
+    const uint gid = get_global_id(0);
+    const uint lid = get_local_id(0);
+    if (gid >= n) {
+        return;
+    }
+
+    __local uint buf[WG_SIZE];
+    if (lid < WG_SIZE) {
+        buf[lid] = as[gid];
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    for (int nVals = WG_SIZE; nVals > 1; nVals /= 2) {
+        if (2 * lid < nVals) {
+            buf[lid] += buf[lid + nVals / 2];
+        }
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+
+    if (lid == 0) {
+        atomic_add(&res[0], buf[0]);
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,7 +1,11 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+#include <string>
 
+#include "cl/sum_cl.h"
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -16,7 +20,13 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 
 int main(int argc, char **argv)
+
 {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
     int benchmarkingIters = 10;
 
     unsigned int reference_sum = 0;
@@ -38,8 +48,8 @@ int main(int argc, char **argv)
             EXPECT_THE_SAME(reference_sum, sum, "CPU result should be consistent!");
             t.nextLap();
         }
-        std::cout << "CPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU:     \t\t" << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "CPU:     \t\t" << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
@@ -53,12 +63,45 @@ int main(int argc, char **argv)
             EXPECT_THE_SAME(reference_sum, sum, "CPU OpenMP result should be consistent!");
             t.nextLap();
         }
-        std::cout << "CPU OMP: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU OMP: " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU OMP: \t\t" << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "CPU OMP: \t\t" << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        uint sum_gpu = 0;
+        const uint wgSize = 128;
+        std::vector<unsigned int> zeros(1, 0);
+
+        ocl::Kernel baseline(sum_kernel, sum_kernel_length, "sum_atomic");
+        ocl::Kernel loop(sum_kernel, sum_kernel_length, "sum_loop"); // 51648.4
+        ocl::Kernel loop_coalesced(sum_kernel, sum_kernel_length, "sum_loop_coalesced");
+        ocl::Kernel local(sum_kernel, sum_kernel_length, "sum_local");
+        ocl::Kernel tree(sum_kernel, sum_kernel_length, "sum_tree");
+
+        ocl::Kernel kernels[5] = {baseline, loop, loop_coalesced, local, tree};
+        uint totalWorks[] = {n, n /4, n / 4, n, n};
+        std::string kernelNames[] = {" (baseline)\t\t", " (loop)\t\t", " (loop_coal)\t\t", " (local_mem)\t\t", " (tree)\t\t"};
+        
+
+        gpu::gpu_mem_32u as_gpu, result;
+        as_gpu.resizeN(n);
+        as_gpu.writeN(as.data(), n);
+        result.resizeN(1);
+
+        for (int kernelIdx = 0; kernelIdx < 5; ++kernelIdx) {
+            timer t;
+            for (int i = 0; i < benchmarkingIters; ++i) {
+                result.writeN(zeros.data(), 1);
+                kernels[kernelIdx].exec(gpu::WorkSize(wgSize, totalWorks[kernelIdx]), as_gpu, result, n);
+
+                result.readN(&sum_gpu, 1);
+                EXPECT_THE_SAME(reference_sum, sum_gpu, "CPU & GPU results must be the same!");
+                t.nextLap();
+            }
+
+            std::cout << "GPU" << kernelNames[kernelIdx] << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU" << kernelNames[kernelIdx] << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
+        }
     }
 }
