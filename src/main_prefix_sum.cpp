822e4becbdf04975fa37cbe3873396c990670a78
Constantine Gamora <kostya.pyatigorsk@gmail.com>
2023-12-13 16:19:13 +0300

    okay

--- src/main_prefix_sum.cpp
+++ src/main_prefix_sum.cpp
@@ -102,12 +102,13 @@ int main(int argc, char **argv) {
 
                 t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-                int work_size = n;
+                int work_size_write = n;
                 for (int p = 0; p <= n_pow; ++p) {
+                    int work_size_reduce = n / (1 << iter);
                     const unsigned int workGroupSize = 128;
-                    prefix_sum_reduce.exec(gpu::WorkSize(work_size < workGroupSize ? work_size : workGroupSize, work_size),
+                    prefix_sum_reduce.exec(gpu::WorkSize(work_size_reduce < workGroupSize ? work_size : workGroupSize, work_size),
                                     as_gpu, p);
-					prefix_sum_write.exec(gpu::WorkSize(work_size < workGroupSize ? work_size : workGroupSize, work_size),
+					prefix_sum_write.exec(gpu::WorkSize(work_size_write < workGroupSize ? work_size : workGroupSize, work_size),
                                     as_gpu, result_gpu, p);
                 }
                 t.nextLap();
