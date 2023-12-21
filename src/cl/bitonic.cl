fe3796fee9cddc29b6d995a527108cc65127653c
Ilia <lelikk2002@gmail.com>
2023-12-21 15:34:26 +0200

    Initial commit

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,3 +1,40 @@
-__kernel void bitonic(__global float *as) {
+#ifdef __CLION_IDE__
+
+#include "clion_defines.cl"
+
+#endif
+
+#line 7
+
+#define WORKGROUP_SIZE 128
+
+__kernel void bitonic(__global float *as, const unsigned int size, const unsigned int block_size) {
     // TODO
+    unsigned int global_id = get_global_id(0);
+    if (global_id * 2 > size) {
+        return;
+    }
+    unsigned int block_id = 2 * global_id / block_size;
+    bool block_type = block_id % 2 == 0;
+
+    unsigned int local_block_size = block_size;
+
+    while (local_block_size >= 2) {
+        if (block_type) {
+            if (as[block_id * local_block_size] > as[block_id * local_block_size + local_block_size / 2]){
+                float tmp = as[block_id * local_block_size];
+                as[block_id * local_block_size] = as[block_id * local_block_size + local_block_size / 2];
+                as[block_id * local_block_size + local_block_size / 2] = tmp;
+            }
+        } else {
+            if (as[block_id * local_block_size] < as[block_id * local_block_size + local_block_size / 2]){
+                float tmp = as[block_id * local_block_size];
+                as[block_id * local_block_size] = as[block_id * local_block_size + local_block_size / 2];
+                as[block_id * local_block_size + local_block_size / 2] = tmp;
+            }
+        }
+        local_block_size /= 2;
+        block_id = 2 * global_id / local_block_size;
+        barrier(CLK_GLOBAL_MEM_FENCE);
+    }
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -1,8 +1,9 @@
-#include <libgpu/context.h>
-#include <libgpu/shared_device_buffer.h>
-#include <libutils/fast_random.h>
-#include <libutils/misc.h>
-#include <libutils/timer.h>
+#include "../libs/gpu/libgpu/context.h"
+#include "../libs/gpu/libgpu/shared_device_buffer.h"
+#include "../libs/utils/libutils/fast_random.h"
+#include "../libs/utils/libutils/misc.h"
+#include "../libs/utils/libutils/timer.h"
+#include "../libs/gpu/libgpu/work_size.h"
 
 // Этот файл будет сгенерирован автоматически в момент сборки - см. convertIntoHeader в CMakeLists.txt:18
 #include "cl/bitonic_cl.h"
@@ -22,6 +23,15 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+#define WORKGROUP_SIZE 128
+
+unsigned int closedPowerOfTwo(unsigned int n) {
+    unsigned int result = 1;
+    while (result < n) {
+        result *= 2;
+    }
+    return result;
+}
 
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
@@ -31,7 +41,7 @@ int main(int argc, char **argv) {
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    unsigned int n = 32;
     std::vector<float> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -50,20 +60,53 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
-    as_gpu.resizeN(n);
+    unsigned int workn = closedPowerOfTwo(n);
+    std::cout << "workn = " << workn << std::endl;
+    as_gpu.resizeN(workn);
+
+    auto as_complete = std::vector<float>(workn);
+
+    for (int i = 0; i < workn; ++i) {
+        if (i < as.size())
+        {
+            as_complete[i] = as[i];
+        }
+        else
+        {
+            as_complete[i] = std::numeric_limits<float>::infinity();
+        }
+    }
+    for (int i = 0; i < as.size(); ++i) {
+        std::cout << as[i] << " ";
+    }
+    std::cout << std::endl;
+
+    for (int i = 0; i < as_complete.size(); ++i) {
+        std::cout << as_complete[i] << " ";
+    }
+    std::cout << std::endl;
 
     {
+        std::cout << "GPU: " << std::endl;
         ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
+        std::cout << "Compiling" << std::endl;
         bitonic.compile();
 
+        std::cout << "Compiled" << std::endl;
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            as_gpu.writeN(as.data(), n);
+            as_gpu.writeN(as_complete.data(), workn);
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
-
+            std::cout << "Started" << std::endl;
+            unsigned int block_size = 2;
+            while (block_size <= n) {
+                bitonic.exec(gpu::WorkSize(WORKGROUP_SIZE, workn), as_gpu, workn, block_size);
+                std::cout << "block_size = " << block_size << std::endl;
+                block_size *= 2;
+            }
             // TODO
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
@@ -76,6 +119,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
