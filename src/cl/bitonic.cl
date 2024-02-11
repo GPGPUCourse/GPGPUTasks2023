f9745dba8ea071d4a9ba182d5a6e199367dd389b
Denis Porsev <den.porsev@gmail.com>
2024-02-12 00:47:59 +0300

    Add bitonic

--- src/cl/bitonic.cl
+++ src/cl/bitonic.cl
@@ -1,3 +1,26 @@
-__kernel void bitonic(__global float *as) {
-    // TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define max(x, y) x > y ? x : y
+#define min(x, y) x < y ? x : y
+
+__kernel void bitonic(__global float *as, unsigned int chunk, unsigned int stride, unsigned int n) {
+    unsigned idx = get_global_id(0);
+    unsigned id2x = idx << 1;
+
+    if (id2x >= n) return;
+    
+    unsigned s = id2x / chunk;
+    idx = s * chunk
+        + (idx - idx / stride * stride) 
+        + (id2x - s * chunk ) / (stride << 1) * (stride << 1);
+
+    float max = max(as[idx], as[idx + stride]);
+    float min = min(as[idx], as[idx + stride]);
+
+    as[idx] = (s & 1) ? max : min;
+    as[idx + stride] = (s & 1) ? min : max;
 }
--- src/main_bitonic.cpp
+++ src/main_bitonic.cpp
@@ -50,7 +50,7 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
 
@@ -58,13 +58,20 @@ int main(int argc, char **argv) {
         ocl::Kernel bitonic(bitonic_kernel, bitonic_kernel_length, "bitonic");
         bitonic.compile();
 
+        const size_t workGroupSize = 128;
+
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            // TODO
+            for (unsigned stride = 2; stride <= n; stride <<= 1) {
+                for (unsigned gap = stride / 2; gap > 0; gap >>= 1) {
+                    bitonic.exec(gpu::WorkSize(workGroupSize, n / 2), as_gpu, stride, gap, n);
+                }
+            }
+            t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
@@ -76,6 +83,6 @@ int main(int argc, char **argv) {
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
